3a3c1a474eba69bf5ed6a8eb017a5d735a6b3803
Shenoy Pratik <sgguruda@amazon.com>
2020-08-14 16:44:55 +0000

    adding common components

---
+++ common/index.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+export const PLUGIN_ID = 'kibanaNotebooks';
+export const PLUGIN_NAME = 'Kibana Notebooks';
+export const API_PREFIX = '/api/notebooks';
+export const SELECTED_BACKEND = 'ZEPPELIN'; // ZEPPELIN || DEFAULT
+
+export const zeppelinURL = 'http://localhost:8080';
+
+export const wreckOptions = {
+  baseUrl: zeppelinURL,
+  headers: { 'Content-Type': 'application/json' },
+};
+
+export interface optionsType {
+  baseUrl: string;
+  payload?: any;
+  headers?: any;
+  redirects?: number;
+  beforeRedirect?: any;
+  redirected?: any;
+  timeout?: number; // default: unlimited
+  maxBytes?: number; // default: unlimited
+  rejectUnauthorized?: boolean;
+  secureProtocol?: string; // The SSL method to use
+  ciphers?: string; // The TLS ciphers to support
+}
