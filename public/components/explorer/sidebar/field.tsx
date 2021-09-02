91193e56a379d9d426cc9efc4abcd0d61b18fa69
Eric Wei <80358241+mengweieric@users.noreply.github.com>
2021-09-02 11:20:37 -0700

    Feature/field search and sorting (#103)
    * left align th text
    
    * fixed row detail expanding issue
    
    * add fields sorting
    
    * added more types and few changes

--- public/components/explorer/sidebar/field.tsx
+++ public/components/explorer/sidebar/field.tsx
@@ -18,27 +18,34 @@ import {
 } from '@elastic/eui';
 import { FieldButton } from '../../common/field_button';
 import { FieldIcon } from '../../common/field_icon';
+import { IField } from '../../../../common/types/explorer';
 
-interface FieldProps {
-  field: string;
+interface IFieldProps {
+  field: IField;
   selected: boolean;
-  onRemoveField?: () => void;
-  onAddField?: () => void;
+  onToggleField: (field: IField) => void;
 }
 
-export const Field = (props: FieldProps) => {
+export const Field = (props: IFieldProps) => {
+
+  const {
+    field,
+    selected,
+    onToggleField
+  } = props;
 
   const [isFieldDetailsOpen, setIsFieldDetailsOpen] = useState(false);
 
-  const addLabelAria = i18n.translate('discover.fieldChooser.discoverField.addButtonAriaLabel', {
+  const addLabelAria = i18n.translate(
+    'addButtonAriaLabel', {
     defaultMessage: 'Add {field} to table',
-    values: { field: props.field.name },
+    values: { field: field.name },
   });
   const removeLabelAria = i18n.translate(
-    'discover.fieldChooser.discoverField.removeButtonAriaLabel',
+    'removeButtonAriaLabel',
     {
       defaultMessage: 'Remove {field} from table',
-      values: { field: props.field.name },
+      values: { field: field.name },
     }
   );
 
@@ -46,12 +53,8 @@ export const Field = (props: FieldProps) => {
     setIsFieldDetailsOpen(!isFieldDetailsOpen);
   }
 
-  const toggleField = (field: string) => {
-    if (props.selected) {
-      props.onRemoveField(field);
-    } else {
-      props.onAddField(field);
-    }
+  const toggleField = (field: IField) => {
+    onToggleField(field);
   };
 
   const getFieldActionDOM = () => {
@@ -60,29 +63,25 @@ export const Field = (props: FieldProps) => {
         delay="long"
         content={
           i18n.translate(
-            props.selected ? 
-            'discover.fieldChooser.discoverField.removeFieldTooltip' :
-            'discover.fieldChooser.discoverField.addFieldTooltip', 
-            {
-              defaultMessage: props.selected ? 'Remove field from table' : 'Add field as column',
-            }
+            selected ? 'removeFieldTooltip' : 'addFieldTooltip', 
+            { defaultMessage: selected ? 'Remove field from table' : 'Add field as column' }
           )
         }
       >
         <EuiButtonIcon
-          color={ props.selected ? "danger" : "primary" }
-          iconType={ props.selected ? "cross": "plusInCircleFilled" }
+          color={ selected ? "danger" : "primary" }
+          iconType={ selected ? "cross": "plusInCircleFilled" }
           className="dscSidebarItem__action"
-          onClick={(ev: React.MouseEvent<HTMLButtonElement>) => {
-            if (ev.type === 'click') {
-              ev.currentTarget.focus();
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+            if (e.type === 'click') {
+              e.currentTarget.focus();
             }
-            ev.preventDefault();
-            ev.stopPropagation();
-            toggleField(props.field);
+            e.preventDefault();
+            e.stopPropagation();
+            toggleField(field);
           }}
-          data-test-subj={`fieldToggle-${props.field.name}`}
-          aria-label={ props.selected ? removeLabelAria : addLabelAria }
+          data-test-subj={`fieldToggle-${field.name}`}
+          aria-label={ selected ? removeLabelAria : addLabelAria }
         />
       </EuiToolTip>
     );
@@ -101,20 +100,20 @@ export const Field = (props: FieldProps) => {
           size="s"
           className="dscSidebarItem"
           isActive={ isFieldDetailsOpen }
-          dataTestSubj={`field-${props.field.name}-showDetails`}
+          dataTestSubj={`field-${field.name}-showDetails`}
           fieldIcon={<FieldIcon
-                      type={props.field.type}
+                      type={field.type}
                     />}
           fieldName={ <span
-                        data-test-subj={`field-${props.field.name}`}
-                        title={props.field.name}
+                        data-test-subj={`field-${field.name}`}
+                        title={field.name}
                         className="dscSidebarField__name"
                       >
-                        { props.field.name }
+                        { field.name }
                       </span> 
                     }
           fieldAction={ getFieldActionDOM() }
-          onClick={() => { togglePopover() }}
+          onClick={() => togglePopover()}
         />
       }
     >
