ba84e8d7edb78f151e5f5fe1dd27940f1cc04031
Kavitha Conjeevaram Mohan <kavithamohan0404@gmail.com>
2021-10-26 10:07:02 -0700

    Update home.tsx

--- public/components/explorer/home.tsx
+++ public/components/explorer/home.tsx
@@ -10,11 +10,9 @@
  */
 
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeQuery } from './slices/query_slice';
-import { initialTabId } from '../../framework/redux/store/shared_state';
+import { useDispatch, batch } from 'react-redux';
+import { uniqueId } from 'lodash';
 import { useHistory } from 'react-router-dom';
-import { selectQueries } from './slices/query_slice';
 import {
   EuiPage,
   EuiPageBody,
@@ -23,22 +21,32 @@ import {
   EuiTitle,
   EuiPageContent,
   EuiListGroup,
-  EuiListGroupItem,
   EuiSpacer,
   EuiFlexGroup,
   EuiFlexItem
 } from '@elastic/eui';
 import { Search } from '../common/search/search';
-import { INDEX, RAW_QUERY } from '../../../common/constants/explorer';
+import { 
+  INDEX,
+  RAW_QUERY,
+  TAB_ID_TXT_PFX,
+  SELECTED_TIMESTAMP,
+  SELECTED_DATE_RANGE
+} from '../../../common/constants/explorer';
 import { useEffect } from 'react';
 import SavedObjects from '../../services/saved_objects/event_analytics/saved_objects';
-import { Table } from './home_table/history_table';
+import { addTab } from './slices/query_tab_slice';
+import { init as initFields } from './slices/field_slice';
+import {
+  init as initQuery,
+  changeQuery
+} from './slices/query_slice';
+import { init as initQueryResult } from './slices/query_result_slice';
 
 interface IHomeProps {
   pplService: any;
   dslService: any;
   savedObjects: SavedObjects;
-  http: any;
 }
 
 export const Home = (props: IHomeProps) => {
@@ -46,14 +54,12 @@ export const Home = (props: IHomeProps) => {
     pplService, 
     dslService,
     savedObjects,
-    http
   } = props;
   const history = useHistory();
   const dispatch = useDispatch();
-  const query = useSelector(selectQueries)[initialTabId][RAW_QUERY];
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [selectedDateRange, setSelectedDateRange] = useState<Array<string>>(['now-15m', 'now']);
   const [savedHistories, setSavedHistories] = useState([]);
-  const hisRef = useRef();
-  hisRef.current = savedHistories;
 
   const fetchHistories = async () => {
     const res = await savedObjects.fetchSavedObjects({
@@ -65,10 +71,49 @@ export const Home = (props: IHomeProps) => {
     setSavedHistories(res['observabilityObjectList'] || []);
   };
 
+  const addNewTab = async () => {
+    //get a new tabId
+    const tabId = uniqueId(TAB_ID_TXT_PFX);
+    
+    // create a new tab
+    await batch(() => {
+      dispatch(initQuery({ tabId, }));
+      dispatch(initQueryResult({ tabId, }));
+      dispatch(initFields({ tabId, }));
+      dispatch(addTab({ tabId, }));
+    });
+
+    return tabId;
+  };
+
   useEffect(() => {
     fetchHistories();
   }, []);
 
+  const addSearchInput = async (tabId: string) => {
+    dispatch(changeQuery({
+      tabId,
+      query: {
+        [RAW_QUERY]: searchQuery,
+        [SELECTED_DATE_RANGE]: selectedDateRange
+      } 
+    }));
+  }
+
+  const handleQuerySearch = async () => {
+    // create new tab
+    const newTabId = await addNewTab();
+
+    // update this new tab with data
+    await addSearchInput(newTabId);
+
+    // redirect to explorer
+    history.push('/event_analytics/explorer');
+  }
+
+  const handleQueryChange = async (query: string, index: string) => setSearchQuery(query);
+
+  const handleTimePickerChange = async (timeRange: Array<string>) => setSelectedDateRange(timeRange);
   return (
     <div className="dscAppContainer">
       <EuiPage>
@@ -84,23 +129,14 @@ export const Home = (props: IHomeProps) => {
       </EuiPage>
       <EuiPageContent>
         <Search 
-          query={ query }
-          handleQueryChange={ (query: string, index: string) => {
-            dispatch(changeQuery({
-              tabId: initialTabId,
-              query: {
-                [RAW_QUERY]: query,
-                [INDEX]: index
-              }
-            }));
-          } }
-          handleQuerySearch={ () => {
-            history.push('/event_analytics/explorer');
-          } }
+          query={ searchQuery }
+          handleQueryChange={ handleQueryChange }
+          handleQuerySearch={ handleQuerySearch }
+          handleTimePickerChange={ handleTimePickerChange }
           pplService={ pplService }
           dslService={ dslService }
-          startTime={ 'now-15m' }
-          endTime={ 'now' }
+          startTime={ selectedDateRange[0] }
+          endTime={ selectedDateRange[1] }
           setStartTime={ () => {} }
           setEndTime={ () => {} }
           setIsOutputStale={ () => {} }
@@ -124,7 +160,6 @@ export const Home = (props: IHomeProps) => {
               </EuiTitle>
               <EuiSpacer size="s" />
             </EuiListGroup>
-            <Table savedHistory={savedHistories} test="stringtest"/>
           </EuiFlexItem>
         </EuiFlexGroup>
       </EuiPageContent>
