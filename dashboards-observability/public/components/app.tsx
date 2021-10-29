21d18e078885e85bbbf331b0fcb986adcd0370d8
Eugene Lee <eugenesk@amazon.com>
2021-10-28 18:41:33 -0700

    Create dummy page Signed-off by: Eugene Lee <eugenesk@amazon.com>

--- dashboards-observability/public/components/app.tsx
+++ dashboards-observability/public/components/app.tsx
@@ -18,7 +18,6 @@ import { observabilityTitle } from '../../common/constants/shared';
 import store from '../framework/redux/store';
 import { AppPluginStartDependencies } from '../types';
 import { Home as ApplicationAnalyticsHome } from './application_analytics/home';
-import { renderPageWithSidebar } from './common/side_nav';
 import { Home as CustomPanelsHome } from './custom_panels/home';
 import { EventAnalytics } from './explorer/event_analytics';
 import { Main as NotebooksHome } from './notebooks/components/main';
@@ -51,11 +50,6 @@ export const App = ({
   const customPanelBreadcrumb = {
     text: 'Operational panels',
     href: '#/operational_panels/',
-  };
-
-  const appAnalyticsBreadcrumb = {
-    text: 'Application analytics',
-    href: '#/application_analytics/',
   };
 
   return (
