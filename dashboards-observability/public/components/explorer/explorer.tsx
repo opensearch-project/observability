f026e1d973a491992e60f93c024c6c4aa5b94251
Eric Wei <80358241+mengweieric@users.noreply.github.com>
2021-11-09 12:39:49 -0800

    Feature autocomplete fix (#208)
    * event sample data
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * home delete and sample data
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * removed one missed
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * separate container
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * removed unused code and some ehchancements
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * autocomplete related fixes
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * home search fix and few cleanups
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * removed comments
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * removed unused reference
    
    Signed-off-by: Eric Wei <menwe@amazon.com>

--- dashboards-observability/public/components/explorer/explorer.tsx
+++ dashboards-observability/public/components/explorer/explorer.tsx
@@ -9,7 +9,7 @@
  * GitHub history for details.
  */
 
-import React, { useState, useMemo, useEffect, useRef } from 'react';
+import React, { useState, useMemo, useEffect, useRef, useCallback } from 'react';
 import { batch, useDispatch, useSelector } from 'react-redux';
 import { 
   uniqueId,
@@ -151,6 +151,7 @@ export const Explorer = ({
   const [isSidebarClosed, setIsSidebarClosed] = useState(false);
   const [timeIntervalOptions, setTimeIntervalOptions] = useState(TIME_INTERVAL_OPTIONS);
   const [isOverridingTimestamp, setIsOverridingTimestamp] = useState(false);
+  const [tempQuery, setTempQuery] = useState('');
   
   const queryRef = useRef();
   const selectedPanelNameRef = useRef();
@@ -352,7 +353,7 @@ export const Explorer = ({
         [SELECTED_DATE_RANGE]: timeRange
       }
     }));
-    fetchData();
+    handleQuerySearch();
   }
 
   /**
@@ -452,7 +453,7 @@ export const Explorer = ({
       }
     }));
 
-    fetchData();
+    handleQuerySearch();
   };
 
   const getMainContent = () => {
@@ -644,16 +645,22 @@ export const Explorer = ({
 
   const handleContentTabClick = (selectedTab: IQueryTab) => setSelectedContentTab(selectedTab.id);
   
-  const handleQuerySearch = () => fetchData();
-
-  const handleQueryChange = async (query: string, index: string) => {
+  const updateQueryInStore = async (query: string) => {
     await dispatch(changeQuery({
       tabId,
       query: {
         [RAW_QUERY]: query.replaceAll(PPL_NEWLINE_REGEX, ''),
-        [INDEX]: index
       },
     }));
+  };
+
+  const handleQuerySearch = useCallback(async () => {
+    await updateQueryInStore(tempQuery);
+    fetchData();
+  }, [tempQuery]);
+
+  const handleQueryChange = async (query: string) => {
+    setTempQuery(query);
   }
 
   const handleSavingObject = async () => {
@@ -772,28 +779,29 @@ export const Explorer = ({
     <div className="dscAppContainer">
       <Search
         key="search-component"
-        query={ query[RAW_QUERY] }
-        handleQueryChange={ handleQueryChange }
-        handleQuerySearch={ () => { handleQuerySearch() } }
-        dslService = { dslService }
-        startTime={ dateRange[0] }
-        endTime={ dateRange[1] }
-        handleTimePickerChange={ (timeRange: Array<string>) => handleTimePickerChange(timeRange) }
-        selectedPanelName={ selectedPanelNameRef.current }
-        selectedCustomPanelOptions={ selectedCustomPanelOptions }
-        setSelectedPanelName={ setSelectedPanelName }
-        setSelectedCustomPanelOptions={ setSelectedCustomPanelOptions }
-        handleSavingObject={ handleSavingObject }
-        isPanelTextFieldInvalid={ isPanelTextFieldInvalid }
-        savedObjects={ savedObjects }
-        showSavePanelOptionsList={ isEqual(selectedContentTabId, TAB_CHART_ID) }
+        query={query[RAW_QUERY]}
+        tempQuery={tempQuery}
+        handleQueryChange={handleQueryChange}
+        handleQuerySearch={handleQuerySearch}
+        dslService = {dslService}
+        startTime={dateRange[0]}
+        endTime={dateRange[1]}
+        handleTimePickerChange={(timeRange: Array<string>) => handleTimePickerChange(timeRange)}
+        selectedPanelName={selectedPanelNameRef.current}
+        selectedCustomPanelOptions={selectedCustomPanelOptions}
+        setSelectedPanelName={setSelectedPanelName}
+        setSelectedCustomPanelOptions={setSelectedCustomPanelOptions}
+        handleSavingObject={handleSavingObject}
+        isPanelTextFieldInvalid={isPanelTextFieldInvalid}
+        savedObjects={savedObjects}
+        showSavePanelOptionsList={isEqual(selectedContentTabId, TAB_CHART_ID)}
       />
       <EuiTabbedContent
         className="mainContentTabs"
-        initialSelectedTab={ memorizedMainContentTabs[0] }
-        selectedTab={ memorizedMainContentTabs.find(tab => tab.id === selectedContentTabId) }
-        onTabClick={ (selectedTab: EuiTabbedContentTab) => handleContentTabClick(selectedTab) }
-        tabs={ memorizedMainContentTabs }
+        initialSelectedTab={memorizedMainContentTabs[0]}
+        selectedTab={memorizedMainContentTabs.find(tab => tab.id === selectedContentTabId)}
+        onTabClick={(selectedTab: EuiTabbedContentTab) => handleContentTabClick(selectedTab)}
+        tabs={memorizedMainContentTabs}
       />
     </div>
   );
