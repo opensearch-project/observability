e00aa22a537062057e781c428721187623fe30ea
Eugene Lee <eugenesk@amazon.com>
2021-11-11 17:21:45 -0800

    Add copyright to all files
    Signed-off-by: Eugene Lee <eugenesk@amazon.com>

--- dashboards-observability/common/constants/trace_analytics.ts
+++ dashboards-observability/common/constants/trace_analytics.ts
@@ -1,12 +1,6 @@
 /*
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- *
- * Modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
  */
 
 export const DATA_PREPPER_INDEX_NAME = 'otel-v1-apm-span-*';
