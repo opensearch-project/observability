806b6cd1e1f454b2a8d92ed9ad7632a7f61308da
Eric Wei <menwe@amazon.com>
2021-11-21 16:19:36 -0800

    resolved failing tests

---
+++ dashboards-observability/public/components/common/test_utils/enzyme_helpers.tsx
@@ -0,0 +1,136 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+/**
+ * Components using the @osd/i18n module require access to the intl context.
+ * This is not available when mounting single components in Enzyme.
+ * These helper functions aim to address that and wrap a valid,
+ * intl context around them.
+ */
+
+import { I18nProvider, InjectedIntl, intlShape, __IntlProvider } from '@osd/i18n/react';
+import { mount, ReactWrapper, render, shallow } from 'enzyme';
+import React, { ReactElement, ValidationMap } from 'react';
+
+// Use fake component to extract `intl` property to use in tests.
+const { intl } = (mount(
+  <I18nProvider>
+    <br />
+  </I18nProvider>
+).find('IntlProvider') as ReactWrapper<{}, {}, __IntlProvider>)
+  .instance()
+  .getChildContext();
+
+function getOptions(context = {}, childContextTypes: ValidationMap<any> = {}, props = {}) {
+  return {
+    context: {
+      ...context,
+      intl,
+    },
+    childContextTypes: {
+      ...childContextTypes,
+      intl: intlShape,
+    },
+    ...props,
+  };
+}
+
+/**
+ * When using @osd/i18n `injectI18n` on components, props.intl is required.
+ */
+function nodeWithIntlProp<T>(node: ReactElement<T>): ReactElement<T & { intl: InjectedIntl }> {
+  return React.cloneElement<any>(node, { intl });
+}
+
+/**
+ *  Creates the wrapper instance using shallow with provided intl object into context
+ *
+ *  @param node The React element or cheerio wrapper
+ *  @param options properties to pass into shallow wrapper
+ *  @return The wrapper instance around the rendered output with intl object in context
+ */
+export function shallowWithIntl<T>(
+  node: ReactElement<T>,
+  {
+    context,
+    childContextTypes,
+    ...props
+  }: {
+    context?: any;
+    childContextTypes?: ValidationMap<any>;
+  } = {}
+) {
+  const options = getOptions(context, childContextTypes, props);
+
+  return shallow(nodeWithIntlProp(node), options);
+}
+
+/**
+ *  Creates the wrapper instance using mount with provided intl object into context
+ *
+ *  @param node The React element or cheerio wrapper
+ *  @param options properties to pass into mount wrapper
+ *  @return The wrapper instance around the rendered output with intl object in context
+ */
+export function mountWithIntl<T>(
+  node: ReactElement<T>,
+  {
+    context,
+    childContextTypes,
+    ...props
+  }: {
+    context?: any;
+    childContextTypes?: ValidationMap<any>;
+  } = {}
+) {
+  const options = getOptions(context, childContextTypes, props);
+
+  return mount(nodeWithIntlProp(node), options);
+}
+
+/**
+ *  Creates the wrapper instance using render with provided intl object into context
+ *
+ *  @param node The React element or cheerio wrapper
+ *  @param options properties to pass into render wrapper
+ *  @return The wrapper instance around the rendered output with intl object in context
+ */
+export function renderWithIntl<T>(
+  node: ReactElement<T>,
+  {
+    context,
+    childContextTypes,
+    ...props
+  }: {
+    context?: any;
+    childContextTypes?: ValidationMap<any>;
+  } = {}
+) {
+  const options = getOptions(context, childContextTypes, props);
+
+  return render(nodeWithIntlProp(node), options);
+}
+
+export const nextTick = () => new Promise((res) => process.nextTick(res));
+
+export function shallowWithI18nProvider<T>(child: ReactElement<T>) {
+  const wrapped = shallow(<I18nProvider>{child}</I18nProvider>);
+  const name = typeof child.type === 'string' ? child.type : child.type.name;
+  return wrapped.find(name).dive();
+}
+
+export function mountWithI18nProvider<T>(child: ReactElement<T>) {
+  const wrapped = mount(<I18nProvider>{child}</I18nProvider>);
+  const name = typeof child.type === 'string' ? child.type : child.type.name;
+  return wrapped.find(name);
+}
