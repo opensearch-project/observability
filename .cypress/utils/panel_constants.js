849be7c26d20904a5024ed2d99c2be1ec8920124
Shenoy Pratik <sgguruda@amazon.com>
2021-12-02 13:13:14 -0800

    Added more cypress tests, updated constants (#282)
    * added more tests, updated constants
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * removed timeout for on clone
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- .cypress/utils/panel_constants.js
+++ .cypress/utils/panel_constants.js
@@ -3,18 +3,34 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const delay = 1500;
+export const delay = 700;
 
 export const TEST_PANEL = 'Test Panel';
+export const SAMPLE_PANEL = '[Logs] Web traffic Panel';
+
+export const SAMPLE_VISUALIZATIONS_NAMES = [
+  '[Logs] Average ram usage by operating systems',
+  '[Logs] Average ram usage per day by apple os',
+  '[Logs] Average ram usage per day by windows os',
+  '[Logs] Daily count for error response codes',
+  '[Logs] Count requests from US to CN, IN and JP',
+  '[Logs] Max and average bytes by host',
+  '[Logs] Count total requests by tags',
+  '[Logs] Daily average bytes',
+];
 
 export const PPL_VISUALIZATIONS = [
   'source = opensearch_dashboards_sample_data_flights | stats count() by Dest',
   'source = opensearch_dashboards_sample_data_flights | stats avg(FlightDelayMin) by Carrier',
+  'source = opensearch_dashboards_sample_data_flights | stats max( DistanceKilometers ) by DestCityName',
 ];
 
 export const PPL_VISUALIZATIONS_NAMES = [
   'Flight count by destination',
   'Average flight delay minutes',
+  'Max distance by destination city',
 ];
+
+export const NEW_VISUALIZATION_NAME = 'Flight count by destination airport';
 
 export const PPL_FILTER = "where Carrier = 'OpenSearch-Air' | where Dest = 'Munich Airport'";
