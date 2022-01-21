e83bae238ae37ecf7bd6f8b5051c53c1a1bfc558
Eugene Lee <eugenesk@amazon.com>
2022-01-21 15:06:02 -0800

    Address comments
    Signed-off-by: Eugene Lee <eugenesk@amazon.com>

--- dashboards-observability/public/components/application_analytics/helpers/utils.tsx
+++ dashboards-observability/public/components/application_analytics/helpers/utils.tsx
@@ -5,5 +5,12 @@
 
 // Name validation 0>Name<=50
 export const isNameValid = (name: string) => {
-  return name.length >= 50 || name.length === 0 ? false : true;
+  let toast: string[]  = [];
+  if (name.length >= 50) {
+    toast.push('Name must be less than 50 characters');
+  }
+  if (name.length === 0) {
+    toast.push('Name must be at least 1 character');
+  }
+  return toast;
 };
