c71ee82c003f685d52afb2a61fef7751d49fa802
Joshua Li <joshuali925@gmail.com>
2022-01-31 23:27:02 +0000

    Add lazy scrolling to event analytics
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- dashboards-observability/public/components/explorer/data_grid.tsx
+++ dashboards-observability/public/components/explorer/data_grid.tsx
@@ -5,10 +5,11 @@
 
 import './data_grid.scss';
 
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useCallback, useEffect, useRef } from 'react';
 import { uniqueId } from 'lodash';
 import { DocViewRow } from './docTable/index';
 import { IExplorerFields, IField } from '../../../common/types/explorer';
+import { PAGE_SIZE } from '../../../common/constants/explorer';
 
 interface DataGridProps {
   rows: Array<any>;
@@ -22,20 +23,44 @@ export function DataGrid(props: DataGridProps) {
     rowsAll,
     explorerFields
   } = props;
+  const [limit, setLimit] = useState(PAGE_SIZE);
+  const loader = useRef<HTMLDivElement>(null);
+
+  const handleObserver = useCallback((entries) => {
+    if (entries[0].isIntersecting) {
+      setLimit((limit) => limit + PAGE_SIZE);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!loader.current || limit >= rows.length) return;
+
+    const observer = new IntersectionObserver(handleObserver, {
+      root: null,
+      rootMargin: '20px',
+      threshold: 0,
+    });
+    observer.observe(loader.current);
+
+    return () => observer.disconnect();
+  }, [limit]);
 
   const getTrs = (
     docs: Array<any> = [],
-    explorerFields: Array<IField>
+    explorerFields: Array<IField>,
+    prevTrs: any[] = [],
   ) => {
-    return docs.map((doc) => {
-      return (
+    const upperLimit = Math.min(limit, docs.length);
+    for (let i = prevTrs.length; i < upperLimit; i++) {
+      prevTrs.push(
         <DocViewRow
           key={ uniqueId('doc_view') } 
-          doc={ doc }
+          doc={ docs[i] }
           selectedCols={ explorerFields }
         />
-      );
-    });
+      )
+    }
+    return prevTrs;
   };
 
   const defaultCols = [
@@ -79,22 +104,20 @@ export function DataGrid(props: DataGridProps) {
     () => getHeaders(explorerFields.queriedFields), 
     [ explorerFields.queriedFields ]
   );
-  const QueriedtableRows = useMemo(
-    () => getTrs(rows, explorerFields.queriedFields),
-    [ rows, explorerFields.queriedFields ]
-  );
+  const [QueriedtableRows, setQueriedtableRows] = useState<any[]>([]);
+  useEffect(() => {
+    setQueriedtableRows((prev) => getTrs(rows, explorerFields.queriedFields, prev))
+  }, [ rows, explorerFields.queriedFields, limit ]);
+
   const headers = useMemo(
     () => getHeaders(explorerFields.selectedFields),
     [ explorerFields.selectedFields ]
   );
-  const tableRows = useMemo(
-    () => {
-      const dataToRender = explorerFields?.queriedFields && explorerFields.queriedFields.length > 0 ? rowsAll : rows
-      return getTrs(dataToRender, explorerFields.selectedFields);
-    },
-    [ rows, explorerFields.selectedFields ]
-  );
-
+  const [tableRows, setTableRows] = useState<any[]>([]);
+  useEffect(() => {
+    const dataToRender = explorerFields?.queriedFields && explorerFields.queriedFields.length > 0 ? rowsAll : rows
+    setTableRows((prev) => getTrs(dataToRender, explorerFields.selectedFields, prev))
+  }, [ rows, explorerFields.selectedFields, limit ]);
 
   return (
     <>
@@ -132,6 +155,7 @@ export function DataGrid(props: DataGridProps) {
           </table>
         )
       }
+      <div ref={loader} />
     </>
   )
 }
