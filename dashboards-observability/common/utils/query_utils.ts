c2080e19f9fde7067eee19cfee2f6b43ee4b5494
Joshua Li <joshuali925@gmail.com>
2022-02-23 21:57:43 +0000

    Revert query pre-processing for parse command
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- dashboards-observability/common/utils/query_utils.ts
+++ dashboards-observability/common/utils/query_utils.ts
@@ -3,26 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { isEmpty } from 'lodash';
-import datemath from '@elastic/datemath';
-import { DATE_PICKER_FORMAT } from '../../common/constants/explorer';
+import datemath from "@elastic/datemath";
+import { isEmpty } from "lodash";
+import { DATE_PICKER_FORMAT } from "../../common/constants/explorer";
 import {
-  PPL_INDEX_REGEX,
   PPL_INDEX_INSERT_POINT_REGEX,
-  PPL_NEWLINE_REGEX
-} from '../../common/constants/shared';
+  PPL_INDEX_REGEX,
+  PPL_NEWLINE_REGEX,
+} from "../../common/constants/shared";
 
-export const getIndexPatternFromRawQuery = (query: string) : string => {
+export const getIndexPatternFromRawQuery = (query: string): string => {
   const matches = query.match(PPL_INDEX_REGEX);
   if (matches) {
     return matches[2];
   }
-  return '';
+  return "";
 };
-
-const commandExists = (query: string, command: string): boolean => {
-  return new RegExp(`\\|\\s*${command}\\b`).test(query);
-}
 
 // insert time filter command and additional commands based on raw query
 export const preprocessQuery = ({
@@ -36,24 +32,23 @@ export const preprocessQuery = ({
   endTime: string;
   timeField?: string;
 }) => {
-
-  let finalQuery = '';
+  let finalQuery = "";
 
   if (isEmpty(rawQuery)) return finalQuery;
-    
+
   // convert to moment
   const start = datemath.parse(startTime)?.format(DATE_PICKER_FORMAT);
   const end = datemath.parse(endTime)?.format(DATE_PICKER_FORMAT);
-  const tokens = rawQuery.replaceAll(PPL_NEWLINE_REGEX, '').match(PPL_INDEX_INSERT_POINT_REGEX);
-  
+  const tokens = rawQuery
+    .replaceAll(PPL_NEWLINE_REGEX, "")
+    .match(PPL_INDEX_INSERT_POINT_REGEX);
+
   if (isEmpty(tokens)) return finalQuery;
-
-  let conditions = `| where ${timeField} >= '${start}' and ${timeField} <= '${end}'`;
-  if (commandExists(rawQuery, 'parse')) {
-    conditions += ` | sort - ${timeField} | head 10000`;
-  }
-
-  finalQuery = `${tokens![1]}=${tokens![2]} ${conditions} ${tokens![3]}`;
+  finalQuery = `${tokens![1]}=${
+    tokens![2]
+  } | where ${timeField} >= '${start}' and ${timeField} <= '${end}'${
+    tokens![3]
+  }`;
 
   return finalQuery;
 };
