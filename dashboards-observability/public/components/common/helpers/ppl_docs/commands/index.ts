658db9a01d7cb02ab8645eb9765a48414d667443
Joshua Li <joshuali925@gmail.com>
2022-03-04 18:35:49 +0000

    Add parse command docs
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- dashboards-observability/public/components/common/helpers/ppl_docs/commands/index.ts
+++ dashboards-observability/public/components/common/helpers/ppl_docs/commands/index.ts
@@ -3,15 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export {dedupCmd} from './dedup';
-export {evalCmd} from './eval';
-export {fieldsCmd} from './fields';
-export {headCmd} from './head';
-export {rareCmd} from './rare';
-export {renameCmd} from './rename';
-export {searchCmd} from './search';
-export {sortCmd} from './sort';
-export {statsCmd} from './stats';
-export {syntaxCmd} from './syntax';
-export {topCmd} from './top';
-export {whereCmd} from './where';
+export { dedupCmd } from './dedup';
+export { evalCmd } from './eval';
+export { fieldsCmd } from './fields';
+export { headCmd } from './head';
+export { parseCmd } from './parse';
+export { rareCmd } from './rare';
+export { renameCmd } from './rename';
+export { searchCmd } from './search';
+export { sortCmd } from './sort';
+export { statsCmd } from './stats';
+export { syntaxCmd } from './syntax';
+export { topCmd } from './top';
+export { whereCmd } from './where';
