30b74616e2bf0563540d6cfbba65d89c1b9cda65
Joshua Li <joshuali925@gmail.com>
2022-03-04 10:03:52 -0800

    Include related services node under service filter (#527)

--- public/components/trace_analytics/components/traces/span_detail_panel.tsx
+++ public/components/trace_analytics/components/traces/span_detail_panel.tsx
@@ -28,8 +28,8 @@ export function SpanDetailPanel(props: {
   colorMap: any;
   page?: string;
   openSpanFlyout?: any;
-  data: { gantt: any[], table: any[], ganttMaxX: number };
-  setData: (data: { gantt: any[], table: any[], ganttMaxX: number }) => void;
+  data?: { gantt: any[]; table: any[]; ganttMaxX: number };
+  setData?: (data: { gantt: any[]; table: any[]; ganttMaxX: number }) => void;
 }) {
   const storedFilters = sessionStorage.getItem('TraceAnalyticsSpanFilters');
   const fromApp = props.page === 'app';
@@ -37,6 +37,17 @@ export function SpanDetailPanel(props: {
     storedFilters ? JSON.parse(storedFilters) : []
   );
   const [DSL, setDSL] = useState<any>({});
+  let data: { gantt: any[]; table: any[]; ganttMaxX: number },
+    setData: (data: { gantt: any[]; table: any[]; ganttMaxX: number }) => void;
+  if (props.data && props.setData) {
+    [data, setData] = [props.data, props.setData];
+  } else {
+    [data, setData] = useState<{ gantt: any[]; table: any[]; ganttMaxX: number }>({
+      gantt: [],
+      table: [],
+      ganttMaxX: 0,
+    });
+  }
 
   const setSpanFiltersWithStorage = (newFilters: Array<{ field: string; value: any }>) => {
     setSpanFilters(newFilters);
@@ -67,7 +78,7 @@ export function SpanDetailPanel(props: {
     if (_.isEmpty(props.colorMap)) return;
     const refreshDSL = spanFiltersToDSL();
     setDSL(refreshDSL);
-    handleSpansGanttRequest(props.traceId, props.http, props.setData, props.colorMap, refreshDSL);
+    handleSpansGanttRequest(props.traceId, props.http, setData, props.colorMap, refreshDSL);
   }, 150);
 
   const spanFiltersToDSL = () => {
@@ -135,10 +146,10 @@ export function SpanDetailPanel(props: {
     };
   };
 
-  const layout = useMemo(() => getSpanDetailLayout(props.data.gantt, props.data.ganttMaxX), [
-    props.data.gantt,
-    props.data.ganttMaxX,
-  ]);
+  const layout = useMemo(
+    () => getSpanDetailLayout(data.gantt, data.ganttMaxX),
+    [data.gantt, data.ganttMaxX]
+  );
 
   const [currentSpan, setCurrentSpan] = useState('');
 
@@ -212,7 +223,7 @@ export function SpanDetailPanel(props: {
       <EuiPanel>
         <EuiFlexGroup>
           <EuiFlexItem>
-            <PanelTitle title="Spans" totalItems={props.data.gantt.length / 2} />
+            <PanelTitle title="Spans" totalItems={data.gantt.length / 2} />
           </EuiFlexItem>
           <EuiFlexItem grow={false}>
             <EuiButtonGroup
@@ -235,7 +246,7 @@ export function SpanDetailPanel(props: {
         <div style={{ overflowY: 'auto', maxHeight: 500 }}>
           {toggleIdSelected === 'timeline' ? (
             <Plt
-              data={props.data.gantt}
+              data={data.gantt}
               layout={layout}
               onClickHandler={onClick}
               onHoverHandler={onHover}
