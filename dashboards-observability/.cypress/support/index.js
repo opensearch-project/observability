114c0de4277c61c2d2cd41d906937e6279d0055f
Shenoy Pratik <sgguruda@amazon.com>
2022-03-04 08:41:56 -0800

    removing comments
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- dashboards-observability/.cypress/support/index.js
+++ dashboards-observability/.cypress/support/index.js
@@ -29,10 +29,3 @@ if (Cypress.env('security_enabled')) {
   Cypress.env('opensearch', 'https://localhost:9200');
 }
 
-// const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
-// Cypress.on('uncaught:exception', (err) => {
-//   /* returning false here prevents Cypress from failing the test */
-//   if (resizeObserverLoopErrRe.test(err.message)) {
-//     return false;
-//   }
-// });
