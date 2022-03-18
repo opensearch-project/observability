071f17e88b89f1cbfc596ad6f667d40e3e5a2e67
Eugene Lee <eugenesk@amazon.com>
2022-03-18 15:27:52 -0700

    modularize and clean up props for traces
    Signed-off-by: Eugene Lee <eugenesk@amazon.com>

--- dashboards-observability/public/components/trace_analytics/components/traces/traces.tsx
+++ dashboards-observability/public/components/trace_analytics/components/traces/traces.tsx
@@ -2,119 +2,25 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-/* eslint-disable react-hooks/exhaustive-deps */
 
-import { EuiSpacer, EuiTitle, PropertySort } from '@elastic/eui';
-import React, { useEffect, useState } from 'react';
+import { EuiBreadcrumb, EuiTitle } from '@elastic/eui';
+import React from 'react';
 import { TraceAnalyticsComponentDeps } from '../../home';
-import { handleTracesRequest } from '../../requests/traces_request_handler';
-import { getValidFilterFields } from '../common/filters/filter_helpers';
-import { filtersToDsl } from '../common/helper_functions';
-import { SearchBar } from '../common/search_bar';
-import { TracesTable } from './traces_table';
+import { TracesContent } from './traces_content';
 
-interface TracesProps extends TraceAnalyticsComponentDeps {
-  appId?: string;
-  appName?: string;
+export interface TracesProps extends TraceAnalyticsComponentDeps {
   page: 'traces' | 'app';
-  openTraceFlyout?: (traceId: string) => void;
-  switchToEditViz?: any;
+  childBreadcrumbs: EuiBreadcrumb[];
+  traceIdColumnAction: any;
 }
 
 export function Traces(props: TracesProps) {
-  const { appId, appName, parentBreadcrumbs, page, openTraceFlyout, switchToEditViz } = props;
-  const [tableItems, setTableItems] = useState([]);
-  const [redirect, setRedirect] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const appTraces = page === 'app';
-
-  const breadCrumbs = appTraces
-    ? [
-        {
-          text: 'Application analytics',
-          href: '#/application_analytics',
-        },
-        {
-          text: `${appName}`,
-          href: `#/application_analytics/${appId}`,
-        },
-      ]
-    : [
-        {
-          text: 'Trace analytics',
-          href: '#/trace_analytics/home',
-        },
-        {
-          text: 'Traces',
-          href: '#/trace_analytics/traces',
-        },
-      ];
-
-  useEffect(() => {
-    props.chrome.setBreadcrumbs([...parentBreadcrumbs, ...breadCrumbs]);
-    const validFilters = getValidFilterFields('traces');
-    props.setFilters([
-      ...props.filters.map((filter) => ({
-        ...filter,
-        locked: validFilters.indexOf(filter.field) === -1,
-      })),
-    ]);
-    setRedirect(false);
-    if (appTraces) {
-      switchToEditViz('');
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!redirect && props.indicesExist) refresh();
-  }, [props.filters, props.appConfigs]);
-
-  const refresh = async (sort?: PropertySort) => {
-    setLoading(true);
-    const DSL = filtersToDsl(
-      props.filters,
-      props.query,
-      props.startTime,
-      props.endTime,
-      props.page,
-      appTraces ? props.appConfigs : []
-    );
-    const timeFilterDSL = filtersToDsl([], '', props.startTime, props.endTime, props.page);
-    await handleTracesRequest(props.http, DSL, timeFilterDSL, tableItems, setTableItems, sort);
-    setLoading(false);
-  };
-
   return (
     <>
-      {appTraces ? (
-        <EuiSpacer size="m" />
-      ) : (
-        <EuiTitle size="l">
-          <h2 style={{ fontWeight: 430 }}>Traces</h2>
-        </EuiTitle>
-      )}
-      <SearchBar
-        query={appTraces ? '' : props.query}
-        filters={props.filters}
-        appConfigs={props.appConfigs}
-        setFilters={props.setFilters}
-        setQuery={props.setQuery}
-        startTime={props.startTime}
-        setStartTime={props.setStartTime}
-        endTime={props.endTime}
-        setEndTime={props.setEndTime}
-        refresh={refresh}
-        page={page}
-      />
-      <EuiSpacer size="m" />
-      <TracesTable
-        items={tableItems}
-        refresh={refresh}
-        indicesExist={props.indicesExist}
-        loading={loading}
-        page={page}
-        openTraceFlyout={openTraceFlyout}
-      />
+      <EuiTitle size="l">
+        <h2 style={{ fontWeight: 430 }}>Traces</h2>
+      </EuiTitle>
+      <TracesContent {...props} />
     </>
   );
 }
