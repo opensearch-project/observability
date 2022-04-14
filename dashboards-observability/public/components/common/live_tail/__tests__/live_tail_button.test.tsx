956a258a11ad71a285fc5eb8249bde466aabf070
Kavitha Conjeevaram Mohan <mohakavi@amazon.com>
2022-04-14 15:10:18 -0700

    change test location
    Signed-off-by: Kavitha Conjeevaram Mohan <mohakavi@amazon.com>

--- dashboards-observability/public/components/common/live_tail/__tests__/live_tail_button.test.tsx
+++ dashboards-observability/public/components/common/live_tail/__tests__/live_tail_button.test.tsx
@@ -6,7 +6,7 @@
 import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { LiveTailButton, StopLiveButton } from './live_tail_button';
+import { LiveTailButton, StopLiveButton } from '../live_tail_button';
 import { waitFor } from '@testing-library/dom';
 
   describe('Live tail button', () => {
