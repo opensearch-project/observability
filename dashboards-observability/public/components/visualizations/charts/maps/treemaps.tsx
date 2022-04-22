03ea5bdacef2a2cad6d511d12bcd6675c51b2166
Mrunal Zambre <mrunal_zambre@persistent.com>
2022-04-22 16:22:58 +0530

    Initial commit for treemap visualization
    Signed-off-by: Mrunal Zambre <mrunal_zambre@persistent.com>

--- dashboards-observability/public/components/visualizations/charts/maps/treemaps.tsx
+++ dashboards-observability/public/components/visualizations/charts/maps/treemaps.tsx
@@ -3,57 +3,85 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
+import React, { useMemo } from 'react';
+import { indexOf, isEmpty, isEqual, isNull, uniq } from 'lodash';
 
 import { Plt } from '../../plotly/plot';
+import { EmptyPlaceholder } from '../../../event_analytics/explorer/visualizations/shared_components/empty_placeholder';
+import { NUMERICAL_FIELDS } from '../../../../../common/constants/shared';
 
-export const TreeMap = ({ visualizations, layout, config }) => {
-  const labels = ['Eve', 'Cain', 'Seth', 'Enos', 'Noam', 'Abel', 'Awan', 'Enoch', 'Azura'];
-  const parents = ['', 'Eve', 'Eve', 'Seth', 'Seth', 'Eve', 'Eve', 'Awan', 'Eve'];
-  const treemapData = [
-    {
-      type: 'treemap',
-      labels,
-      parents,
-      values: [10, 14, 12, 10, 2, 6, 6, 1, 4],
-      textinfo: 'label+value+percent parent+percent entry',
-      domain: { x: [0, 0.48] },
-      outsidetextfont: { size: 20, color: '#377eb8' },
-      marker: { line: { width: 2 } },
-      pathbar: { visible: false },
-    },
-    {
-      type: 'treemap',
-      branchvalues: 'total',
-      labels,
-      parents,
-      domain: { x: [0.52, 1] },
-      values: [65, 14, 12, 10, 2, 6, 6, 1, 4],
-      textinfo: 'label+value+percent parent+percent entry',
-      outsidetextfont: { size: 20, color: '#377eb8' },
-      marker: { line: { width: 2 } },
-      pathbar: { visible: false },
-    },
-  ];
-  const finalLayout = {
-    annotations: [
+export const TreeMap = ({ visualizations, layout, config }: any) => {
+  const {
+    data,
+    metadata: { fields },
+  } = visualizations.data.rawVizData;
+  const { dataConfig = {}, layoutConfig = {} } = visualizations?.data?.userConfigs;
+
+  const labelField =
+    dataConfig?.valueOptions &&
+    dataConfig?.valueOptions.labelField &&
+    !isEmpty(dataConfig?.valueOptions.labelField)
+      ? dataConfig?.valueOptions.labelField[0]
+      : fields[fields.length - 1];
+
+  const parentField =
+    dataConfig?.valueOptions &&
+    dataConfig?.valueOptions.parentField &&
+    !isEmpty(dataConfig?.valueOptions.parentField)
+      ? dataConfig?.valueOptions.parentField[0]
+      : null;
+
+  const valueField =
+    dataConfig?.valueOptions &&
+    dataConfig?.valueOptions.valueField &&
+    !isEmpty(dataConfig?.valueOptions.valueField)
+      ? dataConfig?.valueOptions.valueField[0]
+      : fields[0];
+
+  if (
+    isEmpty(data[labelField.name]) ||
+    isEmpty(data[valueField.name]) ||
+    (!isNull(parentField) && isEmpty(data[parentField.name])) ||
+    isEqual(labelField, parentField) ||
+    indexOf(NUMERICAL_FIELDS, valueField.type) < 0
+  )
+    return <EmptyPlaceholder icon={visualizations?.vis?.iconType} />;
+
+  const treemapData = useMemo(() => {
+    let labelsArray, parentsArray, valuesArray;
+
+    if (parentField === null) {
+      labelsArray = [...data[labelField.name]];
+      parentsArray = [...Array(labelsArray.length).fill('')];
+      valuesArray = [...data[valueField.name]];
+    } else {
+      const uniqueParents = uniq(data[parentField.name]);
+      labelsArray = [...data[labelField.name], ...uniqueParents];
+      parentsArray = [...data[parentField.name], ...Array(uniqueParents.length).fill('')];
+      valuesArray = [...data[valueField.name], ...Array(uniqueParents.length).fill(0)];
+    }
+
+    return [
       {
-        showarrow: false,
-        text: 'branchvalues: <b>remainder</b>',
-        x: 0.25,
-        xanchor: 'center',
-        y: 1.1,
-        yanchor: 'bottom',
+        type: 'treemap',
+        labels: labelsArray,
+        parents: parentsArray,
+        values: valuesArray,
+        textinfo: 'label+value+percent parent+percent entry',
       },
-      {
-        showarrow: false,
-        text: 'branchvalues: <b>total</b>',
-        x: 0.75,
-        xanchor: 'center',
-        y: 1.1,
-        yanchor: 'bottom',
-      },
-    ],
+    ];
+  }, [data, labelField, valueField, parentField]);
+
+  const mergedLayout = {
+    ...layout,
+    ...(layoutConfig.layout && layoutConfig.layout),
+    title: dataConfig?.panelOptions?.title || layoutConfig.layout?.title || '',
   };
-  return <Plt data={treemapData} layout={finalLayout} config={config} />;
+
+  const mergedConfigs = {
+    ...config,
+    ...(layoutConfig.config && layoutConfig.config),
+  };
+
+  return <Plt data={treemapData} layout={mergedLayout} config={mergedConfigs} />;
 };
