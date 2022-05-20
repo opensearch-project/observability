c7ce173a227e4594433a5bb9ab5e3d8f1973b706
Eugene Lee <eugenesk@amazon.com>
2022-05-20 08:57:56 -0700

    Combine into one application type
    Signed-off-by: Eugene Lee <eugenesk@amazon.com>

--- dashboards-observability/common/types/app_analytics.ts
+++ dashboards-observability/common/types/app_analytics.ts
@@ -7,22 +7,26 @@ export interface OptionType {
   label: string;
 }
 
-export interface ApplicationListType {
-  name: string;
-  id: string;
-  panelId: string;
-  composition: string[];
-  dateCreated: string;
-  dateModified: string;
-  availability: { name: string; color: string; mainVisId: string };
-}
+// export interface ApplicationListType {
+//   name: string;
+//   id: string;
+//   panelId: string;
+//   composition: string[];
+//   dateCreated: string;
+//   dateModified: string;
+//   availability: { name: string; color: string; mainVisId: string };
+// }
 
 export interface ApplicationType {
+  id: string;
   name: string;
+  dateCreated: string;
+  dateModified: string;
   description: string;
+  composition: string[];
   baseQuery: string;
   servicesEntities: string[];
   traceGroups: string[];
   panelId: string;
-  availabilityVisId: string;
+  availability: { name: string; color: string; mainVisId: string };
 }
