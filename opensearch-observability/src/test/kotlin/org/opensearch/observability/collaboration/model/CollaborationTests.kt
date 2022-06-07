4f6ce03c475ddbccd3874ea4ffc47aa3b775abd1
Abbas Hussain <abbas_10690@yahoo.com>
2022-06-07 10:30:26 -0700

    Add unit tests
    Signed-off-by: Abbas Hussain <abbas_10690@yahoo.com>

---
+++ opensearch-observability/src/test/kotlin/org/opensearch/observability/collaboration/model/CollaborationTests.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.observability.collaboration.model
+
+import com.fasterxml.jackson.core.JsonParseException
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.opensearch.commons.utils.recreateObject
+import org.opensearch.observability.createObjectFromJsonString
+import org.opensearch.observability.getJsonString
+import org.opensearch.observability.collaboration.model.Collaboration.TextInfo
+
+internal class CollaborationTests {
+    private val sampleCollaboration= Collaboration(
+      CollaborationDataType.TEXT,
+      listOf("dev", "test"),
+      false,
+      TextInfo("bcdbhbk56dcd", "5c4cds6c6cd", "298d6eewcecwcdewcc",)
+    )
+
+    @Test
+    fun `Collaboration serialize and deserialize transport object should be equal`() {
+        val recreatedObject = recreateObject(sampleCollaboration) { Collaboration(it) }
+        assertEquals(sampleCollaboration, recreatedObject)
+    }
+
+    @Test
+    fun `Collaboration serialize and deserialize using json object should be equal`() {
+        val jsonString = getJsonString(sampleCollaboration)
+        val recreatedObject = createObjectFromJsonString(jsonString) { Collaboration.parse(it) }
+        assertEquals(sampleCollaboration, recreatedObject)
+    }
+
+    @Test
+    fun `Collaboration should deserialize json object using parser`() {
+        val jsonString =
+                "{\"type\":\"TEXT\",\"text\":{\"pageId\":\"scdsc5646c\",\"paragraphId\":\"hdjcknj656\",\"lineId\":\"cjdnjk5665cdcd\"},\"tags\":[\"dev\",\"latency\"],\"resolved\":false}"
+        val recreatedObject = createObjectFromJsonString(jsonString) { Collaboration.parse(it) }
+        assertEquals(sampleCollaboration, recreatedObject)
+    }
+
+    @Test
+    fun `Collaboration should throw exception when invalid json object is passed`() {
+        val jsonString = "sample message"
+        assertThrows<JsonParseException> {
+          createObjectFromJsonString(jsonString) { Collaboration.parse(it) }
+        }
+    }
+    
+    @Test
+    fun `Collaboration should safely ignore extra field in json object`() {
+        val jsonString =
+                "{\"type\":\"TEXT\",\"text\":{\"pageId\":\"scdsc5646c\",\"paragraphId\":\"hdjcknj656\",\"lineId\":\"cjdnjk5665cdcd\"},\"name\":\"Abbas\",\"tags\":[\"dev\",\"latency\"],\"resolved\":false}"
+        val recreatedObject = createObjectFromJsonString(jsonString) { Collaboration.parse(it) }
+        assertEquals(sampleCollaboration, recreatedObject)
+    }
+}
---
+++ opensearch-observability/src/test/kotlin/org/opensearch/observability/collaboration/model/CommentTests.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.observability.collaboration.model
+
+import com.fasterxml.jackson.core.JsonParseException
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.opensearch.commons.utils.recreateObject
+import org.opensearch.observability.createObjectFromJsonString
+import org.opensearch.observability.getJsonString
+
+internal class CommentTests {
+    private val sampleComment = Comment(
+        "hnjkchjr565f6cvd",
+        "This is a sample comment"
+    )
+
+    @Test
+    fun `Comment serialize and deserialize transport object should be equal`() {
+        val recreatedObject = recreateObject(sampleComment) { Comment(it) }
+        assertEquals(sampleComment, recreatedObject)
+    }
+
+    @Test
+    fun `Comment serialize and deserialize using json object should be equal`() {
+        val jsonString = getJsonString(sampleComment)
+        val recreatedObject = createObjectFromJsonString(jsonString) { Collaboration.parse(it) }
+        assertEquals(sampleComment, recreatedObject)
+    }
+
+    @Test
+    fun `Comment should deserialize json object using parser`() {
+        val jsonString =
+                "{\"collaborationId\":\"TEXT\",\"text\":\"Is this spike related to new 1.4 deployment? josh@ can you check\"}"
+        val recreatedObject = createObjectFromJsonString(jsonString) { Comment.parse(it) }
+        assertEquals(sampleComment, recreatedObject)
+    }
+
+    @Test
+    fun `Comment should throw exception when invalid json object is passed`() {
+        val jsonString = "sample message"
+        assertThrows<JsonParseException> {
+          createObjectFromJsonString(jsonString) { Comment.parse(it) }
+        }
+    }
+
+    @Test
+    fun `Comment should safely ignore extra field in json object`() {
+        val jsonString =
+                "{\"collaborationId\":\"TEXT\",\"text\":\"Is this spike related to new 1.4 deployment? josh@ can you check\",\"region\":\"us-east-1\"}"
+        val recreatedObject = createObjectFromJsonString(jsonString) { Comment.parse(it) }
+        assertEquals(sampleComment, recreatedObject)
+    }
+}
