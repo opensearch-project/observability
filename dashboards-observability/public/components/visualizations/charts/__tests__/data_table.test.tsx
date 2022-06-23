546130e41d74fe7f42aeeaf85c973bdb93f1cc3a
Ramneet Chopra <ramneet_chopra@persistent.com>
2022-06-23 09:10:04 +0000

    test case of data_table updated
    Signed-off-by: Ramneet Chopra <ramneet_chopra@persistent.com>

--- dashboards-observability/public/components/visualizations/charts/__tests__/data_table.test.tsx
+++ dashboards-observability/public/components/visualizations/charts/__tests__/data_table.test.tsx
@@ -3,27 +3,24 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { waitFor } from '@testing-library/react';
 import { DataTable } from '../data_table/data_table';
 import { TEST_VISUALIZATIONS_DATA } from '../../../../../test/event_analytics_constants';
+import { AgGridReact } from 'ag-grid-react';
 
 describe('Data table component', () => {
   configure({ adapter: new Adapter() });
 
   it('Renders data table component', async () => {
-    const wrapper = mount(
-      <DataTable
-        visualizations={TEST_VISUALIZATIONS_DATA}
-      />
-    );
-
-    wrapper.update();
-
+    const gridWrapper = shallow(<DataTable visualizations={TEST_VISUALIZATIONS_DATA} />);
+    const agGridReactObj = gridWrapper.find(AgGridReact);
+    agGridReactObj.simulate('gridReady')
+    expect(agGridReactObj).toBeTruthy();
     await waitFor(() => {
-      expect(wrapper).toMatchSnapshot();
+      expect(gridWrapper).toMatchSnapshot();
     });
   });
 });
