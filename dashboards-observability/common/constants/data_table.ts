e7edb3f0c8182c29bfc76ffd1e7a3868b2545a43
Ramneet Chopra <ramneet_chopra@persistent.com>
2022-06-23 14:51:35 +0000

    grid height issue:fixed
    Signed-off-by: Ramneet Chopra <ramneet_chopra@persistent.com>
    Signed-off-by: abasatwar <amruta_basatwar@persistent.com>

--- dashboards-observability/common/constants/data_table.ts
+++ dashboards-observability/common/constants/data_table.ts
@@ -4,8 +4,6 @@
  */
 
 export const GRID_HEADER_COLUMN_MAX_WIDTH = '150px';
-export const GRID_HEIGHT = '460px';
-export const GRID_HEIGHT_FULLSCREEN = '500px';
 export const GRID_PAGE_RANGE_DSIPLAY = 5;
 export const COLUMN_DEFAULT_MIN_WIDTH = 100;
 export const GRID_PAGE_SIZES = [10, 50, 100];
