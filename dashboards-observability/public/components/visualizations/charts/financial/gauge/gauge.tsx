8550434adc02b416502b052fc5d49bfcb0400f9d
Abhay Pandey <abhay_pandey@persistent.com>
2022-09-16 19:07:56 +0530

    first
    Signed-off-by: Abhay Pandey <abhay_pandey@persistent.com>

--- dashboards-observability/public/components/visualizations/charts/financial/gauge/gauge.tsx
+++ dashboards-observability/public/components/visualizations/charts/financial/gauge/gauge.tsx
@@ -10,6 +10,7 @@ import { PLOTLY_GAUGE_COLUMN_NUMBER } from '../../../../../../common/constants/e
 import { DefaultGaugeChartParameters } from '../../../../../../common/constants/explorer';
 import { ThresholdUnitType } from '../../../../event_analytics/explorer/visualizations/config_panel/config_panes/config_controls/config_thresholds';
 import { EmptyPlaceholder } from '../../../../event_analytics/explorer/visualizations/shared_components/empty_placeholder';
+import { find, isEmpty } from 'lodash';
 
 const {
   GaugeTitleSize,
@@ -27,11 +28,9 @@ export const Gauge = ({ visualizations, layout, config }: any) => {
 
   // data config parametrs
   const { dataConfig = {}, layoutConfig = {} } = visualizations.data.userConfigs;
-  const dimensions = dataConfig?.dimensions
-    ? dataConfig.dimensions.filter((item) => item.name !== '')
-    : [];
+   
   const metrics = dataConfig?.metrics ? dataConfig.metrics.filter((item) => item.name !== '') : [];
-  const dimensionsLength = dimensions.length;
+  
   const metricsLength = metrics.length;
   const numberOfGauges = dataConfig?.panelOptions?.numberOfGauges || DisplayDefaultGauges;
 
@@ -44,40 +43,56 @@ export const Gauge = ({ visualizations, layout, config }: any) => {
   const orientation = dataConfig?.chartStyles?.orientation || OrientationDefault;
   const legendPlacement = dataConfig?.chartStyles?.legendPlacement || LegendPlacement;
 
-  const isEmptyPlot = !metricsLength;
+  const isEmptyPlot = !metricsLength || isEmpty(data);
+
+  const getAggValue = (metric:any) => { 
+    return metric.alias ? metric.alias : metric.aggregation + '' + '(' +metric.name+ ')'
+  };
 
   if (isEmptyPlot) return <EmptyPlaceholder icon={visualizations?.vis?.icontype} />;
+  
+  const dimensions = useMemo(() => {  
+      // span selection
+      const timestampField = find(fields, (field) => field.type === 'timestamp');
+
+      const dataCon = dataConfig?.dimensions
+      ? dataConfig.dimensions.filter((item) => item.name !== '')
+      : [];
+
+      if (dataConfig.span && dataConfig.span.time_field && timestampField && dataCon.length === 0 ) {
+        return [timestampField, ...dataConfig.dimensions];
+      }
+  
+      return [...dataConfig.dimensions];
+    }, [dataConfig.dimensions]);
+  
+  const dimensionsLength = dimensions.length;
 
   const gaugeData: Plotly.Data[] = useMemo(() => {
     let calculatedGaugeData: Plotly.Data[] = [];
     if (dimensionsLength || metricsLength) {
-      // case 1,2: no dimension, single/multiple metrics
-      if (!dimensionsLength && metricsLength >= 1) {
-        calculatedGaugeData = metrics.map((metric: any) => {
-          return {
-            field_name: metric.name,
-            value: data[metric.name][0],
-          };
-        });
-      }
-
+      // case 1 and 2 is removed because dimension is not blank in any case.
       // case 3: multiple dimensions and multiple metrics
+
       if (dimensionsLength && metricsLength) {
         const selectedDimensionsData = dimensions
-          .map((dimension: any) => data[dimension.name].slice(0, numberOfGauges))
+          .map((dimension: any) => { 
+              return data[dimension.name] ? data[dimension.name].slice(0, numberOfGauges): [];
+          })
           .reduce((prev, cur) => {
             return prev.map((i, j) => `${i}, ${cur[j]}`);
           });
-        const selectedMetricsData = metrics.map((metric: any) =>
-          data[`${metric.aggregation}(${metric.name})`].slice(0, numberOfGauges)
-        );
+        const selectedMetricsData = metrics.map((metric: any) => {
+          const val  =  getAggValue(metric);
+          return data[`${val}`] ? data[`${val}`].slice(0, numberOfGauges) : [];
+        });
 
         selectedMetricsData.map((metricSlice: any, metricSliceIndex: number) => {
           calculatedGaugeData = [
             ...calculatedGaugeData,
             ...metricSlice.map((metricSliceData: any, metricSliceDataIndex: number) => {
               return {
-                field_name: `${selectedDimensionsData[metricSliceDataIndex]}, ${metrics[metricSliceIndex].name}`,
+                field_name: `${selectedDimensionsData[metricSliceDataIndex]}, ${getAggValue(metrics[metricSliceIndex])}`,
                 value: metricSliceData,
               };
             }),
@@ -183,6 +198,5 @@ export const Gauge = ({ visualizations, layout, config }: any) => {
     ...config,
     ...(layoutConfig.config && layoutConfig.config),
   };
-
   return <Plt data={gaugeData} layout={mergedLayout} config={mergedConfigs} />;
 };
