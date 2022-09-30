f69e0efe62ae9d0614452095c6d8ea2316e906e6
Joshua Li <joshuali925@gmail.com>
2022-09-30 17:29:30 +0000

    Fix jacoco
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- opensearch-observability/src/test/kotlin/org/opensearch/observability/PluginRestTestCase.kt
+++ opensearch-observability/src/test/kotlin/org/opensearch/observability/PluginRestTestCase.kt
@@ -39,7 +39,6 @@ import java.io.IOException
 import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
-import java.nio.file.Paths
 import java.security.cert.X509Certificate
 import javax.management.MBeanServerInvocationHandler
 import javax.management.ObjectName
