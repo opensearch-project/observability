e9ff79aa85c95b2325364118c69d335870401c3c
saisanju-s <43745923+saisanju-s@users.noreply.github.com>
2022-10-01 03:01:57 +0530

    [Bug]: Data Configuration panel for Logs view remains disabled. (#1057)
    * Working on review comments
    
    Signed-off-by: ruchika-narang <ruchika_narang@persistent.com>
    
    * Replacing series and dimension with dynamic naming
    
    Signed-off-by: ruchika-narang <ruchika_narang@persistent.com>
    
    * Revert code for histogram and treemap after antlr merge because of crashes in app
    
    Signed-off-by: ruchika-narang <ruchika_narang@persistent.com>
    
    * Working on review comments, renaming metrics to series
    
    Signed-off-by: ruchika-narang <ruchika_narang@persistent.com>
    
    * Removing extra import after rebasing main
    
    Signed-off-by: ruchika-narang <ruchika_narang@persistent.com>
    
    * Working on review comments
    
    Signed-off-by: ruchika-narang <ruchika_narang@persistent.com>
    
    * Replacing series and dimension with dynamic naming
    
    Signed-off-by: ruchika-narang <ruchika_narang@persistent.com>
    
    * Fixed logs view visualizations issues.
    
    Signed-off-by: Saisanju Sreevalsakumar <saisanju_s@persistent.com>
    
    * Resolved review comments for log view fixes 1.
    
    Signed-off-by: Saisanju Sreevalsakumar <saisanju_s@persistent.com>
    
    Signed-off-by: ruchika-narang <ruchika_narang@persistent.com>
    Signed-off-by: Saisanju Sreevalsakumar <saisanju_s@persistent.com>
    Co-authored-by: ruchika-narang <ruchika_narang@persistent.com>

--- dashboards-observability/public/components/event_analytics/explorer/explorer.tsx
+++ dashboards-observability/public/components/event_analytics/explorer/explorer.tsx
@@ -897,6 +897,35 @@ export const Explorer = ({
           [GROUPBY]: [{ bucketSize: '', bucketOffset: '' }],
           [AGGREGATIONS]: [],
         };
+      case VIS_CHART_TYPES.LogsView: {
+        const dimensions = statsToken.aggregations
+          .map((agg) => {
+            const logViewField = `${agg.function.name}(${agg.function.value_expression})` ?? '';
+            return {
+              label: logViewField,
+              name: logViewField,
+            };
+          })
+          .concat(
+            groupByToken.group_fields?.map((agg) => ({
+              label: agg.name ?? '',
+              name: agg.name ?? '',
+            }))
+          );
+        if (span !== undefined) {
+          const { time_field, interval, unit } = span;
+          const timespanField = `span(${time_field[0].name},${interval}${unit[0].value})`;
+          dimensions.push({
+            label: timespanField,
+            name: timespanField,
+          });
+        }
+        return {
+          [AGGREGATIONS]: [],
+          [GROUPBY]: dimensions,
+        };
+      }
+
       default:
         return {
           [AGGREGATIONS]: statsToken.aggregations.map((agg) => ({
