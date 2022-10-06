a1e4a98e0a02a239d70053e877bc035abc84717f
Koustubh Karmalkar <koustubh_karmalkar@persistent.com>
2022-10-06 16:26:53 +0530

    Fixed [BUG]: Visualization height panel issue for all charts
    Signed-off-by: Koustubh Karmalkar <koustubh_karmalkar@persistent.com>

--- dashboards-observability/public/components/event_analytics/explorer/visualizations/shared_components/empty_placeholder.tsx
+++ dashboards-observability/public/components/event_analytics/explorer/visualizations/shared_components/empty_placeholder.tsx
@@ -8,16 +8,20 @@ import { EuiIcon, EuiText, EuiSpacer } from '@elastic/eui';
 import { FormattedMessage } from '@osd/i18n/react';
 import './empty_placeholder.scss';
 
-export const EmptyPlaceholder = (props: {icon: string}) => (
+export const EmptyPlaceholder = (props: { icon: string }) => (
   <>
-    <EuiText className="lnsChart__empty" textAlign="center" color="subdued" size="xs" data-test-subj="vizWorkspace__noData">
+    <EuiText
+      className="lnsChart__empty"
+      textAlign="center"
+      color="subdued"
+      size="xs"
+      data-test-subj="vizWorkspace__noData"
+      style={{ height: '100%' }}
+    >
       <EuiIcon type={props.icon} color="subdued" size="xxl" />
       <EuiSpacer size="l" />
       <p>
-        <FormattedMessage
-          id="visualization_noData"
-          defaultMessage="No results found"
-        />
+        <FormattedMessage id="visualization_noData" defaultMessage="No results found" />
       </p>
     </EuiText>
   </>
