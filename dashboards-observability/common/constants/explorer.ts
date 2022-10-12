a8289552046d5808a64e4442927e70b69641d21d
Ramneet Chopra <ramneet_chopra@persistent.com>
2022-10-12 11:14:23 +0530

    unsed lines of code removed
    Signed-off-by: Ramneet Chopra <ramneet_chopra@persistent.com>

--- dashboards-observability/common/constants/explorer.ts
+++ dashboards-observability/common/constants/explorer.ts
@@ -238,7 +238,6 @@ export interface DefaultBarChartStylesProps {
   BarMode: string;
   GroupWidth: number;
   BarWidth: number;
-  LineWidth: number;
   LabelSize: number;
 }
 
@@ -246,7 +245,6 @@ export const DEFAULT_BAR_CHART_STYLES: DefaultBarChartStylesProps = {
   BarMode: 'group',
   GroupWidth: 0.7,
   BarWidth: 0.97,
-  LineWidth: 1,
   LabelSize: 12,
 };
 
@@ -271,17 +269,3 @@ export enum ConfigChartOptionsEnum {
 
 export const CUSTOM_LABEL = 'customLabel';
 export const BREAKDOWNS = 'breakdowns';
-
-export interface DefaultBarChartStylesProps {
-  BarMode: string;
-  GroupWidth: number;
-  BarWidth: number;
-  LabelSize: number;
-}
-
-export const DEFAULT_BAR_CHART_STYLES: DefaultBarChartStylesProps = {
-  BarMode: 'group',
-  GroupWidth: 0.7,
-  BarWidth: 0.97,
-  LabelSize: 12,
-};
