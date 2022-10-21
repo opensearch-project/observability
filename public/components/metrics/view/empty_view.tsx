e60c7d50b885e260b89e1fd61c9ee145f3dfd42c
Shenoy Pratik <sgguruda@amazon.com>
2022-10-20 22:51:56 -0700

    added empty view
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- public/components/metrics/view/empty_view.tsx
+++ public/components/metrics/view/empty_view.tsx
@@ -3,22 +3,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { EuiSpacer, EuiText, EuiIcon, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { EuiSpacer, EuiText, EuiIcon, EuiFlexGroup, EuiFlexItem, EuiPanel } from '@elastic/eui';
 import React, { useState } from 'react';
+import './empty_view.scss';
 
 export const EmptyMetricsView = () => {
   return (
     <div>
-      <EuiSpacer size="xxl" />
-      <EuiText textAlign="center">
-        <EuiIcon type="minusInCircle" size="xxl" />
-        <EuiSpacer size="s" />
-        <h2>No Metrics Selected</h2>
-        <EuiSpacer size="m" />
-        <EuiText color="subdued" size="m">
-          Select a metric from the left sidepanel to view results.
-        </EuiText>
-      </EuiText>
+      <EuiPanel className="empty-view-metrics">
+        <div className="align-center-view">
+          <EuiText>
+            <EuiIcon type="minusInCircle" size="xxl" />
+            <EuiSpacer size="s" />
+            <h2>No Metrics Selected</h2>
+            <EuiSpacer size="m" />
+            <EuiText color="subdued" size="m">
+              Select a metric from the left sidepanel to view results.
+            </EuiText>
+          </EuiText>
+        </div>
+      </EuiPanel>
     </div>
   );
 };
