e36c75861df5c0c8190f6327716ac20f4d409826
Sean Li <lnse@amazon.com>
2022-11-01 09:33:20 -0700

    Adding API connects
    Signed-off-by: Sean Li <lnse@amazon.com>

--- dashboards-observability/common/constants/metrics.ts
+++ dashboards-observability/common/constants/metrics.ts
@@ -3,9 +3,5 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const SELECTED_METRICS = 'selectedMetrics';
-export const RECENTLY_CREATED_METRICS = 'unselectedMetrics';
-export const AVAILABLE_METRICS = 'availableMetrics';
-
 // redux
 export const REDUX_SLICE_METRICS = 'metrics';
--- dashboards-observability/public/components/metrics/sidebar/sidebar.tsx
+++ dashboards-observability/public/components/metrics/sidebar/sidebar.tsx
@@ -5,73 +5,43 @@
 
 import './sidebar.scss';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { cloneDeep } from 'lodash';
 import { EuiTitle, EuiSpacer, EuiAccordion, EuiLink } from '@elastic/eui';
 import { I18nProvider } from '@osd/i18n/react';
-import { batch, useDispatch } from 'react-redux';
-import { AVAILABLE_METRICS, SELECTED_METRICS } from '../../../../common/constants/metrics';
-import { updateMetrics, sortMetrics, selectMetrics } from '../redux/slices/metrics_slice';
+import { batch, useDispatch, useSelector } from 'react-redux';
+import {
+  availableMetricsSelector,
+  deSelectMetric,
+  selectMetric,
+  loadMetrics,
+  selectedMetricsSelector,
+} from '../redux/slices/metrics_slice';
+import { CoreStart } from '../../../../../../src/core/public';
+import PPLService from '../../../services/requests/ppl';
 
 interface ISidebarProps {
-  metricsList: any;
-  visualizationsList: any;
+  http: CoreStart['http'];
+  pplService: PPLService;
 }
 
 export const Sidebar = (props: ISidebarProps) => {
+  const { http, pplService } = props;
   const dispatch = useDispatch();
 
-  // Initializing sidebar with dummy data
-  const { metricsList, visualizationsList } = props;
-  const metricNames = metricsList.metrics;
-  const [showFields, setShowFields] = useState<boolean>(false);
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const availableMetrics = useSelector(availableMetricsSelector);
+  const selectedMetrics = useSelector(selectedMetricsSelector);
 
-  /**
-   * Toggle metric names between selected and available sets
-   * @param metric metric to be toggled
-   * @param MetricSetToRemove set where this metric needs to be removed from
-   * @param MetricSetToAdd set where this metric needs to be added to
-   */
-  const toggleMetrics = (
-    metric: any,
-    visualization: boolean,
-    MetricSetToRemove: string,
-    MetricSetToAdd: string
-  ) => {
-    const nextMetrics = cloneDeep(metricsList);
-    const thisMetricSet = nextMetrics.metrics[MetricSetToRemove];
-    // Dummy data for now, will globalize the filter later
-    // If statements are to separate visualizations and dummy data from prometheus
-    let nextMetricSet;
-    if (visualization) {
-      nextMetricSet = thisMetricSet.filter((row: any) => row.name !== metric.name);
-    } else {
-      nextMetricSet = thisMetricSet.filter((row: any) => row[2] !== metric[2]);
-    }
-    nextMetrics.metrics[MetricSetToRemove] = nextMetricSet;
-    if (!visualization || MetricSetToRemove !== SELECTED_METRICS) {
-      nextMetrics.metrics[MetricSetToAdd].push(metric);
-    }
+  useEffect(() => {
     batch(() => {
-      dispatch(
-        updateMetrics({
-          data: { ...nextMetrics.metrics },
-        })
-      );
-      dispatch(
-        sortMetrics({
-          data: [MetricSetToAdd],
-        })
-      );
+      dispatch(loadMetrics({ http, pplService }));
     });
-  };
+  }, []);
 
-  const handleAddMetric = (metric: any, visualization: boolean) =>
-    toggleMetrics(metric, visualization, AVAILABLE_METRICS, SELECTED_METRICS);
+  const handleAddMetric = (metric: any) => dispatch(selectMetric(metric));
 
-  const handleRemoveMetric = (metric: any, visualization: boolean) => {
-    toggleMetrics(metric, visualization, SELECTED_METRICS, AVAILABLE_METRICS);
+  const handleRemoveMetric = (metric: any) => {
+    dispatch(deSelectMetric(metric));
   };
 
   return (
@@ -99,29 +69,11 @@ export const Sidebar = (props: ISidebarProps) => {
           paddingSize="xs"
         >
           <ul>
-            {metricNames[SELECTED_METRICS].map((metric: any) => {
-              let name;
-              if (metric.name) {
-                name = metric.name;
-              } else {
-                name = metric[2];
-              }
-              return (
-                <li>
-                  <EuiLink
-                    onClick={() => {
-                      if (metric.name) {
-                        handleRemoveMetric(metric, true);
-                      } else {
-                        handleRemoveMetric(metric, false);
-                      }
-                    }}
-                  >
-                    {name}
-                  </EuiLink>
-                </li>
-              );
-            })}
+            {selectedMetrics.map((metric: any) => (
+              <li key={metric.id}>
+                <EuiLink onClick={() => handleRemoveMetric(metric)}>{metric.name}</EuiLink>
+              </li>
+            ))}
           </ul>
         </EuiAccordion>
         <EuiSpacer size="s" />
@@ -136,32 +88,11 @@ export const Sidebar = (props: ISidebarProps) => {
           paddingSize="xs"
         >
           <ul>
-            {visualizationsList.map((visualization: any) => {
-              return (
-                <li>
-                  <EuiLink
-                    onClick={() => {
-                      handleAddMetric(visualization, true);
-                    }}
-                  >
-                    {visualization.name}
-                  </EuiLink>
-                </li>
-              );
-            })}
-            {metricNames[AVAILABLE_METRICS].map((metric: any) => {
-              return (
-                <li>
-                  <EuiLink
-                    onClick={() => {
-                      handleAddMetric(metric, false);
-                    }}
-                  >
-                    {metric[2]}
-                  </EuiLink>
-                </li>
-              );
-            })}
+            {availableMetrics.map((metric: any) => (
+              <li key={metric.id}>
+                <EuiLink onClick={() => handleAddMetric(metric)}>{metric.name}</EuiLink>
+              </li>
+            ))}
           </ul>
         </EuiAccordion>
       </section>
