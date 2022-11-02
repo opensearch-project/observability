e672f289ac1ff1a4f0c9fd4d6eda40181f4c577a
Shenoy Pratik <sgguruda@amazon.com>
2022-11-02 09:53:40 -0700

    Adding visualizations panel to metrics (#1222)
    * adding vizualization components
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * added metrics layout to redux
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * removed comments and updated constants
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * remove excess console.log
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- dashboards-observability/public/components/metrics/helpers/utils.tsx
+++ dashboards-observability/public/components/metrics/helpers/utils.tsx
@@ -2,7 +2,6 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-/* eslint-disable no-console */
 
 import dateMath from '@elastic/datemath';
 import { ShortDate } from '@elastic/eui';
@@ -11,9 +10,13 @@ import _ from 'lodash';
 import { Moment } from 'moment-timezone';
 import React from 'react';
 import { CUSTOM_PANELS_API_PREFIX } from '../../../../common/constants/custom_panels';
-import { PPL_DATE_FORMAT, PPL_INDEX_REGEX } from '../../../../common/constants/shared';
+import { PPL_DATE_FORMAT } from '../../../../common/constants/shared';
 import PPLService from '../../../services/requests/ppl';
 import { CoreStart } from '../../../../../../src/core/public';
+import { MetricType } from '../../../../common/types/metrics';
+import { Layout } from 'react-grid-layout';
+import { VisualizationType } from '../../../../common/types/custom_panels';
+import { DEFAULT_METRIC_HEIGHT, DEFAULT_METRIC_WIDTH } from '../../../../common/constants/metrics';
 
 export const convertDateTime = (datetime: string, isStart = true, formatted = true) => {
   let returnTime: undefined | Moment;
@@ -57,4 +60,109 @@ export const getVisualizations = (http: CoreStart['http']) => {
   return http.get(`${CUSTOM_PANELS_API_PREFIX}/visualizations/`).catch((err) => {
     console.error('Issue in fetching all saved visualizations', err);
   });
+};
+
+interface boxType {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+const calculatOverlapArea = (bb1: boxType, bb2: boxType) => {
+  const x_left = Math.max(bb1.x1, bb2.x1);
+  const y_top = Math.max(bb1.y1, bb2.y1);
+  const x_right = Math.min(bb1.x2, bb2.x2);
+  const y_bottom = Math.min(bb1.y2, bb2.y2);
+
+  if (x_right < x_left || y_bottom < y_top) return 0;
+  return (x_right - x_left) * (y_bottom - y_top);
+};
+
+const getTotalOverlapArea = (panelVisualizations: MetricType[]) => {
+  const newVizBox = { x1: 0, y1: 0, x2: DEFAULT_METRIC_WIDTH, y2: DEFAULT_METRIC_HEIGHT };
+  const currentVizBoxes = panelVisualizations.map((visualization) => {
+    return {
+      x1: visualization.x,
+      y1: visualization.y,
+      x2: visualization.x + visualization.w,
+      y2: visualization.y + visualization.h,
+    };
+  });
+
+  let isOverlapping = 0;
+  currentVizBoxes.map((viz) => {
+    isOverlapping += calculatOverlapArea(viz, newVizBox);
+  });
+  return isOverlapping;
+};
+
+// We want to check if the new visualization being added, can be placed at { x: 0, y: 0, w: 6, h: 4 };
+// To check this we try to calculate overlap between all the current visualizations and new visualization
+// if there is no overalap (i.e Total Overlap Area is 0), we place the new viz. in default position
+// else, we add it to the bottom of the panel
+export const getNewVizDimensions = (panelVisualizations: MetricType[]) => {
+  let maxY: number = 0;
+  let maxYH: number = 0;
+
+  // check if we can place the new visualization at default location
+  if (getTotalOverlapArea(panelVisualizations) === 0) {
+    return { x: 0, y: 0, w: DEFAULT_METRIC_WIDTH, h: DEFAULT_METRIC_HEIGHT };
+  }
+
+  // else place the new visualization at the bottom of the panel
+  panelVisualizations.map((panelVisualization: MetricType) => {
+    if (panelVisualization.y >= maxY) {
+      maxY = panelVisualization.y;
+      maxYH = panelVisualization.h;
+    }
+  });
+
+  return { x: 0, y: maxY + maxYH, w: DEFAULT_METRIC_WIDTH, h: DEFAULT_METRIC_HEIGHT };
+};
+
+export const getMinSpanInterval = (start: any, end: any) => {
+  const momentStart = dateMath.parse(start)!;
+  const momentEnd = dateMath.parse(end, { roundUp: true })!;
+  const diffSeconds = momentEnd.unix() - momentStart.unix();
+  let minInterval;
+  // // less than 1 second
+  // if (diffSeconds <= 1) minInterval = 'ms';
+  // less than 2 minutes
+  if (diffSeconds <= 60 * 2) minInterval = 's';
+  // less than 2 hours
+  else if (diffSeconds <= 3600 * 2) minInterval = 'm';
+  // less than 2 days
+  else if (diffSeconds <= 86400 * 2) minInterval = 'h';
+  // less than 1 month
+  else if (diffSeconds <= 86400 * 31) minInterval = 'd';
+  // less than 3 months
+  else if (diffSeconds <= 86400 * 93) minInterval = 'w';
+  // less than 1 year
+  else if (diffSeconds <= 86400 * 366) minInterval = 'M';
+
+  return minInterval;
+};
+
+// Merges new layout into visualizations
+export const mergeLayoutAndMetrics = (
+  layout: Layout[],
+  newVisualizationList: VisualizationType[]
+) => {
+  const newPanelVisualizations: VisualizationType[] = [];
+
+  for (let i = 0; i < newVisualizationList.length; i++) {
+    for (let j = 0; j < layout.length; j++) {
+      if (newVisualizationList[i].id == layout[j].i) {
+        newPanelVisualizations.push({
+          ...newVisualizationList[i],
+          x: layout[j].x,
+          y: layout[j].y,
+          w: layout[j].w,
+          h: layout[j].h,
+        });
+      }
+    }
+  }
+  return newPanelVisualizations;
 };
--- dashboards-observability/public/components/metrics/index.tsx
+++ dashboards-observability/public/components/metrics/index.tsx
@@ -8,33 +8,26 @@ import {
   EuiButtonIcon,
   EuiPage,
   EuiPageBody,
-  EuiSpacer,
-  EuiSuperDatePicker,
-  EuiSuperDatePickerProps,
-  EuiText,
-  EuiTitle,
-  EuiTabbedContent,
-  OnRefreshChangeProps,
-  OnRefreshProps,
+  htmlIdGenerator,
   OnTimeChangeProps,
   ShortDate,
-  EuiPageContentBody,
-  EuiFlexGroup,
-  EuiFlexItem,
 } from '@elastic/eui';
 import { DurationRange } from '@elastic/eui/src/components/date_picker/types';
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, RouteComponentProps } from 'react-router-dom';
 import classNames from 'classnames';
-import { useSelector } from 'react-redux';
 import { StaticContext } from 'react-router-dom';
-import { CUSTOM_PANELS_API_PREFIX } from ' ../../../common/constants/custom_panels';
-import { uiSettingsService } from '../../../common/utils';
 import { ChromeBreadcrumb, CoreStart } from '../../../../../src/core/public';
 import { onTimeChange } from './helpers/utils';
 import { Sidebar } from './sidebar/sidebar';
 import { EmptyMetricsView } from './view/empty_view';
 import PPLService from '../../services/requests/ppl';
+import { TopMenu } from './top_menu/top_menu';
+import { MetricType } from '../../../common/types/metrics';
+import { MetricsGrid } from './view/metrics_grid';
+import { useSelector } from 'react-redux';
+import { metricsLayoutSelector, selectedMetricsSelector } from './redux/slices/metrics_slice';
+import { resolutionOptions } from '../../../common/constants/metrics';
 
 interface MetricsProps {
   http: CoreStart['http'];
@@ -45,29 +38,70 @@ interface MetricsProps {
 }
 
 export const Home = ({ http, chrome, parentBreadcrumb, renderProps, pplService }: MetricsProps) => {
+  // Redux tools
+  const selectedMetrics = useSelector(selectedMetricsSelector);
+  const metricsLayout = useSelector(metricsLayoutSelector);
+
   // Date picker constants
   const [recentlyUsedRanges, setRecentlyUsedRanges] = useState<DurationRange[]>([]);
-  const [start, setStart] = useState<ShortDate>('now-30m');
-  const [end, setEnd] = useState<ShortDate>('now');
-  const [dateDisabled, setDateDisabled] = useState(false);
+  const [startTime, setStartTime] = useState<ShortDate>('now-1d');
+  const [endTime, setEndTime] = useState<ShortDate>('now');
+
+  // Top panel
+  const [IsTopPanelDisabled, setIsTopPanelDisabled] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [onRefresh, setOnRefresh] = useState(false);
+  const [editActionType, setEditActionType] = useState('');
+  const [resolutionValue, setResolutionValue] = useState(resolutionOptions[2].value);
+  const [spanValue, setSpanValue] = useState(1);
+  const resolutionSelectId = htmlIdGenerator('resolutionSelect')();
 
   // Side bar constants
   const [isSidebarClosed, setIsSidebarClosed] = useState(false);
 
-  // Date Picker functions
-  // Empty functions for now
-  const onRefreshFilters = (startTime: ShortDate, endTime: ShortDate) => {};
+  // Metrics constants
+  const [panelVisualizations, setPanelVisualizations] = useState<MetricType[]>([]);
+
+  const onRefreshFilters = (startTime: ShortDate, endTime: ShortDate) => {
+    setOnRefresh(!onRefresh);
+  };
+
   const onDatePickerChange = (props: OnTimeChangeProps) => {
     onTimeChange(
       props.start,
       props.end,
       recentlyUsedRanges,
       setRecentlyUsedRanges,
-      setStart,
-      setEnd
+      setStartTime,
+      setEndTime
     );
     onRefreshFilters(props.start, props.end);
   };
+
+  const onEditClick = (savedVisualizationId: string) => {
+    window.location.assign(`#/event_analytics/explorer/${savedVisualizationId}`);
+  };
+
+  const onSideBarClick = () => {
+    setIsSidebarClosed((staleState) => {
+      return !staleState;
+    });
+    setTimeout(function () {
+      window.dispatchEvent(new Event('resize'));
+    }, 300);
+  };
+
+  useEffect(() => {
+    selectedMetrics.length > 0 ? setIsTopPanelDisabled(false) : setIsTopPanelDisabled(true);
+  }, [selectedMetrics]);
+
+  useEffect(() => {
+    setPanelVisualizations(metricsLayout);
+  }, [metricsLayout]);
+
+  useEffect(() => {
+    if (editMode) setIsTopPanelDisabled(true);
+  }, [editMode]);
 
   const mainSectionClassName = classNames({
     'col-md-9': !isSidebarClosed,
@@ -83,18 +117,23 @@ export const Home = ({ http, chrome, parentBreadcrumb, renderProps, pplService }
           <div>
             <EuiPage>
               <EuiPageBody component="div">
-                <EuiFlexGroup gutterSize="s" justifyContent="flexEnd">
-                  <EuiFlexItem grow={false}>
-                    <EuiSuperDatePicker
-                      dateFormat={uiSettingsService.get('dateFormat')}
-                      start={start}
-                      end={end}
-                      onTimeChange={onDatePickerChange}
-                      recentlyUsedRanges={recentlyUsedRanges}
-                      isDisabled={dateDisabled}
-                    />
-                  </EuiFlexItem>
-                </EuiFlexGroup>
+                <TopMenu
+                  IsTopPanelDisabled={IsTopPanelDisabled}
+                  startTime={startTime}
+                  endTime={endTime}
+                  onDatePickerChange={onDatePickerChange}
+                  recentlyUsedRanges={recentlyUsedRanges}
+                  editMode={editMode}
+                  setEditMode={setEditMode}
+                  setEditActionType={setEditActionType}
+                  panelVisualizations={panelVisualizations}
+                  setPanelVisualizations={setPanelVisualizations}
+                  resolutionValue={resolutionValue}
+                  setResolutionValue={setResolutionValue}
+                  spanValue={spanValue}
+                  setSpanValue={setSpanValue}
+                  resolutionSelectId={resolutionSelectId}
+                />
                 <div className="dscAppContainer">
                   <div className="col-md-3 dscSidebar__container dscCollapsibleSidebar">
                     <div className="">
@@ -103,11 +142,7 @@ export const Home = ({ http, chrome, parentBreadcrumb, renderProps, pplService }
                         iconType={isSidebarClosed ? 'menuRight' : 'menuLeft'}
                         iconSize="m"
                         size="s"
-                        onClick={() => {
-                          setIsSidebarClosed((staleState) => {
-                            return !staleState;
-                          });
-                        }}
+                        onClick={() => onSideBarClick()}
                         data-test-subj="collapseSideBarButton"
                         aria-controls="discover-sidebar"
                         aria-expanded={isSidebarClosed ? 'false' : 'true'}
@@ -117,11 +152,24 @@ export const Home = ({ http, chrome, parentBreadcrumb, renderProps, pplService }
                     </div>
                   </div>
                   <div className={`dscWrapper ${mainSectionClassName}`}>
-                    <div className="dscWrapper__content">
-                      <div className="dscResults">
-                        <EmptyMetricsView />
-                      </div>
-                    </div>
+                    {selectedMetrics.length > 0 ? (
+                      <MetricsGrid
+                        http={http}
+                        chrome={chrome}
+                        panelVisualizations={panelVisualizations}
+                        setPanelVisualizations={setPanelVisualizations}
+                        editMode={editMode}
+                        pplService={pplService}
+                        startTime={startTime}
+                        endTime={endTime}
+                        moveToEvents={onEditClick}
+                        onRefresh={onRefresh}
+                        editActionType={editActionType}
+                        spanParam={spanValue + resolutionValue}
+                      />
+                    ) : (
+                      <EmptyMetricsView />
+                    )}
                   </div>
                 </div>
               </EuiPageBody>
