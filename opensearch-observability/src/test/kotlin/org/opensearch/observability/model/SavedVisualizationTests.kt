40b83bd3d0a28d9948271a89ac355fc5e792e623
Kavitha Conjeevaram Mohan <mohakavi@amazon.com>
2022-11-02 18:07:22 -0700

    Show metrics in events homepage (#1232)
    * Add Metrics to events home page and fix jest test
    
    Signed-off-by: Kavitha Conjeevaram Mohan <mohakavi@amazon.com>
    
    * fix save_query_table jest test
    
    Signed-off-by: Kavitha Conjeevaram Mohan <mohakavi@amazon.com>
    
    * fix backend test
    
    Signed-off-by: Kavitha Conjeevaram Mohan <mohakavi@amazon.com>
    
    Signed-off-by: Kavitha Conjeevaram Mohan <mohakavi@amazon.com>

--- opensearch-observability/src/test/kotlin/org/opensearch/observability/model/SavedVisualizationTests.kt
+++ opensearch-observability/src/test/kotlin/org/opensearch/observability/model/SavedVisualizationTests.kt
@@ -32,10 +32,7 @@ internal class SavedVisualizationTests {
         "KE1Ie34BbsTr-CsB4G6Y",
         "{\"dataConfig\":\"{}\",\"layoutConfig\":\"{}\"}",
         "metric",
-        "hours (h)",
-        SavedVisualization.SelectedLabels(
-            listOf(SavedVisualization.Token("avg"))
-        )
+        "hours (h)"
     )
 
     @Test
@@ -54,7 +51,7 @@ internal class SavedVisualizationTests {
     @Test
     fun `SavedVisualization should deserialize json object using parser`() {
         val jsonString =
-            "{\"name\":\"test-saved-visualization\",\"description\":\"test description\",\"query\":\"source=index | where utc_time > timestamp('2021-07-01 00:00:00') and utc_time < timestamp('2021-07-02 00:00:00')\",\"type\":\"bar\",\"selected_date_range\":{\"start\":\"now/15m\",\"end\":\"now\",\"text\":\"utc_time > timestamp('2021-07-01 00:00:00') and utc_time < timestamp('2021-07-02 00:00:00')\"},\"selected_timestamp\":{\"name\":\"utc_time\",\"type\":\"timestamp\"},\"selected_fields\":{\"text\":\"| fields clientip, bytes, memory, host\",\"tokens\":[{\"name\":\"utc_time\",\"type\":\"timestamp\"}]},\"application_id\":\"KE1Ie34BbsTr-CsB4G6Y\",\"user_configs\":\"{\\\"dataConfig\\\":\\\"{}\\\",\\\"layoutConfig\\\":\\\"{}\\\"}\",\"sub_type\":\"metric\",\"units_of_measure\":\"hours (h)\",\"labels\":[{\"label\":\"avg\"}]}"
+            "{\"name\":\"test-saved-visualization\",\"description\":\"test description\",\"query\":\"source=index | where utc_time > timestamp('2021-07-01 00:00:00') and utc_time < timestamp('2021-07-02 00:00:00')\",\"type\":\"bar\",\"selected_date_range\":{\"start\":\"now/15m\",\"end\":\"now\",\"text\":\"utc_time > timestamp('2021-07-01 00:00:00') and utc_time < timestamp('2021-07-02 00:00:00')\"},\"selected_timestamp\":{\"name\":\"utc_time\",\"type\":\"timestamp\"},\"selected_fields\":{\"text\":\"| fields clientip, bytes, memory, host\",\"tokens\":[{\"name\":\"utc_time\",\"type\":\"timestamp\"}]},\"application_id\":\"KE1Ie34BbsTr-CsB4G6Y\",\"user_configs\":\"{\\\"dataConfig\\\":\\\"{}\\\",\\\"layoutConfig\\\":\\\"{}\\\"}\",\"sub_type\":\"metric\",\"units_of_measure\":\"hours (h)\"}"
         val recreatedObject = createObjectFromJsonString(jsonString) { SavedVisualization.parse(it) }
         assertEquals(sampleSavedVisualization, recreatedObject)
     }
@@ -70,7 +67,7 @@ internal class SavedVisualizationTests {
     @Test
     fun `SavedVisualization should safely ignore extra field in json object`() {
         val jsonString =
-            "{\"name\":\"test-saved-visualization\",\"description\":\"test description\",\"query\":\"source=index | where utc_time > timestamp('2021-07-01 00:00:00') and utc_time < timestamp('2021-07-02 00:00:00')\",\"type\":\"bar\",\"selected_date_range\":{\"start\":\"now/15m\",\"end\":\"now\",\"text\":\"utc_time > timestamp('2021-07-01 00:00:00') and utc_time < timestamp('2021-07-02 00:00:00')\"},\"selected_timestamp\":{\"name\":\"utc_time\",\"type\":\"timestamp\"},\"selected_fields\":{\"text\":\"| fields clientip, bytes, memory, host\",\"tokens\":[{\"name\":\"utc_time\",\"type\":\"timestamp\"}]},\"application_id\":\"KE1Ie34BbsTr-CsB4G6Y\",\"user_configs\":\"{\\\"dataConfig\\\":\\\"{}\\\",\\\"layoutConfig\\\":\\\"{}\\\"}\",\"sub_type\":\"metric\",\"units_of_measure\":\"hours (h)\",\"labels\":[{\"label\":\"avg\"}]}"
+            "{\"name\":\"test-saved-visualization\",\"description\":\"test description\",\"query\":\"source=index | where utc_time > timestamp('2021-07-01 00:00:00') and utc_time < timestamp('2021-07-02 00:00:00')\",\"type\":\"bar\",\"selected_date_range\":{\"start\":\"now/15m\",\"end\":\"now\",\"text\":\"utc_time > timestamp('2021-07-01 00:00:00') and utc_time < timestamp('2021-07-02 00:00:00')\"},\"selected_timestamp\":{\"name\":\"utc_time\",\"type\":\"timestamp\"},\"selected_fields\":{\"text\":\"| fields clientip, bytes, memory, host\",\"tokens\":[{\"name\":\"utc_time\",\"type\":\"timestamp\"}]},\"application_id\":\"KE1Ie34BbsTr-CsB4G6Y\",\"user_configs\":\"{\\\"dataConfig\\\":\\\"{}\\\",\\\"layoutConfig\\\":\\\"{}\\\"}\",\"sub_type\":\"metric\",\"units_of_measure\":\"hours (h)\"}"
         val recreatedObject = createObjectFromJsonString(jsonString) { SavedVisualization.parse(it) }
         assertEquals(sampleSavedVisualization, recreatedObject)
     }
