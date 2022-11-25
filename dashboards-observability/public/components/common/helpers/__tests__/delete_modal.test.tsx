16ba6b927a534f65c6351b85c0bc6c655ddb44e3
Ramneet Chopra <ramneet_chopra@persistent.com>
2022-11-25 15:28:56 +0000

    common helps tests updated
    Signed-off-by: Ramneet Chopra <ramneet_chopra@persistent.com>

--- dashboards-observability/public/components/common/helpers/__tests__/delete_modal.test.tsx
+++ dashboards-observability/public/components/common/helpers/__tests__/delete_modal.test.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { waitFor } from '@testing-library/react';
@@ -24,6 +24,25 @@ describe('Delete modal component', () => {
 
     wrapper.update();
 
+    await waitFor(() => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  it('Renders Delete modal and check for events', async () => {
+    const wrapper = shallow(
+      <DeleteModal
+        title="Delete"
+        message="Action can't be undone"
+        onCancel={() => jest.fn()}
+        onConfirm={() => jest.fn()}
+      />
+    );
+
+    wrapper
+      .find('[data-test-subj="popoverModal__deleteTextInput"]')
+      .simulate('change', { target: { value: 'delete' } });
+    wrapper.find('[data-test-subj="popoverModal__deleteButton"]').simulate('click', 'junk');
     await waitFor(() => {
       expect(wrapper).toMatchSnapshot();
     });
--- dashboards-observability/public/components/common/helpers/__tests__/ppl_refrence_flyout.test.tsx
+++ dashboards-observability/public/components/common/helpers/__tests__/ppl_refrence_flyout.test.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { waitFor } from '@testing-library/react';
@@ -13,10 +13,10 @@ describe('PPL reference flyout component', () => {
   configure({ adapter: new Adapter() });
 
   it('Renders PPL reference flyout', async () => {
-    const wrapper = mount(<PPLReferenceFlyout module="explorere" closeFlyout={() => {}} />);
-
+    const wrapper = mount(<PPLReferenceFlyout module="explorere" closeFlyout={() => jest.fn()} />);
     wrapper.update();
-
+    wrapper.find('input').simulate('change', { target: { value: 'stats' } });
+    wrapper.find('.euiMark').simulate('click');
     await waitFor(() => {
       expect(wrapper).toMatchSnapshot();
     });
