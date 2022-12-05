be8b274bc6065e62d89838f4682a68661e76093e
Derek Ho <dxho@amazon.com>
2022-12-05 16:31:09 -0500

    update a test
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- dashboards-observability/public/components/notebooks/components/__tests__/note_table.test.tsx
+++ dashboards-observability/public/components/notebooks/components/__tests__/note_table.test.tsx
@@ -81,4 +81,39 @@ describe('<NoteTable /> spec', () => {
     utils.getByText('Actions').click();
     utils.getByText('Rename').click();
   });
+
+  it('create notebook', () => {
+    const fetchNotebooks = jest.fn();
+    const addSampleNotebooks = jest.fn();
+    const createNotebook = jest.fn();
+    const renameNotebook = jest.fn();
+    const cloneNotebook = jest.fn();
+    const deleteNotebook = jest.fn();
+    const setBreadcrumbs = jest.fn();
+    const setToast = jest.fn();
+    const notebooks = Array.from({ length: 5 }, (v, k) => ({
+      path: `path-${k}`,
+      id: `id-${k}`,
+      dateCreated: 'date-created',
+      dateModified: 'date-modified',
+    }));
+    const utils = render(
+      <NoteTable
+        loading={false}
+        fetchNotebooks={fetchNotebooks}
+        addSampleNotebooks={addSampleNotebooks}
+        notebooks={notebooks}
+        createNotebook={createNotebook}
+        renameNotebook={renameNotebook}
+        cloneNotebook={cloneNotebook}
+        deleteNotebook={deleteNotebook}
+        parentBreadcrumb={{ href: 'parent-href', text: 'parent-text' }}
+        setBreadcrumbs={setBreadcrumbs}
+        setToast={setToast}
+      />
+    );
+    expect(utils.container.firstChild).toMatchSnapshot();
+    utils.getByText('Create notebook').click();
+    expect(utils.container.firstChild).toMatchSnapshot();
+  });
 });
