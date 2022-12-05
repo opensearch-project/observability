fe06f8c9a28a6d14a64acc0bf89266b5d3693ebe
Joshua Li <joshuali925@gmail.com>
2022-12-05 10:47:10 -0800

    Fix tests
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- opensearch-observability/src/test/kotlin/org/opensearch/observability/metrics/MetricsTests.kt
+++ opensearch-observability/src/test/kotlin/org/opensearch/observability/metrics/MetricsTests.kt
@@ -8,53 +8,8 @@ package org.opensearch.observability.metrics
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.opensearch.observability.model.ObservabilityObjectType
-import kotlin.test.assertEquals
 
 internal class MetricsTests {
-    @Test
-    fun incrementCreateCounter() {
-        Metrics.incrementObservabilityObjectActionCounter(ObservabilityObjectType.NOTEBOOK, Metrics.Action.CREATE)
-        Metrics.incrementObservabilityObjectActionCounter(ObservabilityObjectType.APPLICATION, Metrics.Action.CREATE)
-        Metrics.incrementObservabilityObjectActionCounter(ObservabilityObjectType.SAVED_QUERY, Metrics.Action.CREATE)
-        Metrics.incrementObservabilityObjectActionCounter(
-            ObservabilityObjectType.OPERATIONAL_PANEL,
-            Metrics.Action.CREATE
-        )
-        Metrics.incrementObservabilityObjectActionCounter(
-            ObservabilityObjectType.SAVED_VISUALIZATION,
-            Metrics.Action.CREATE
-        )
-        Metrics.incrementObservabilityObjectActionCounter(ObservabilityObjectType.TIMESTAMP, Metrics.Action.CREATE)
-        assertEquals(Metrics.NOTEBOOK_CREATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.APPLICATION_CREATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.SAVED_QUERY_CREATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.OPERATIONAL_PANEL_CREATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.SAVED_VISUALIZATION_CREATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.TIMESTAMP_CREATE_TOTAL.counter.value, 1L)
-    }
-
-    @Test
-    fun incrementUpdateCounter() {
-        Metrics.incrementObservabilityObjectActionCounter(ObservabilityObjectType.NOTEBOOK, Metrics.Action.UPDATE)
-        Metrics.incrementObservabilityObjectActionCounter(ObservabilityObjectType.APPLICATION, Metrics.Action.UPDATE)
-        Metrics.incrementObservabilityObjectActionCounter(ObservabilityObjectType.SAVED_QUERY, Metrics.Action.UPDATE)
-        Metrics.incrementObservabilityObjectActionCounter(
-            ObservabilityObjectType.OPERATIONAL_PANEL,
-            Metrics.Action.UPDATE
-        )
-        Metrics.incrementObservabilityObjectActionCounter(
-            ObservabilityObjectType.SAVED_VISUALIZATION,
-            Metrics.Action.UPDATE
-        )
-        Metrics.incrementObservabilityObjectActionCounter(ObservabilityObjectType.TIMESTAMP, Metrics.Action.UPDATE)
-        assertEquals(Metrics.NOTEBOOK_UPDATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.APPLICATION_UPDATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.SAVED_QUERY_UPDATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.OPERATIONAL_PANEL_UPDATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.SAVED_VISUALIZATION_UPDATE_TOTAL.counter.value, 1L)
-        assertEquals(Metrics.TIMESTAMP_UPDATE_TOTAL.counter.value, 1L)
-    }
-
     @Test
     fun testInvalidArguments() {
         assertDoesNotThrow {
