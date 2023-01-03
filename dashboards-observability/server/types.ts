595dbc73ef90f5467db1e0c46933e88449226970
Derek Ho <dxho@amazon.com>
2023-01-03 12:43:43 -0500

    remove front end code from backend repo
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- dashboards-observability/server/types.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ObservabilityPluginSetup {}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ObservabilityPluginStart {}
