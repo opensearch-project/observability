39f8e63b01e64cee8d54755851ff61bf41b441ce
Derek Ho <dxho@amazon.com>
2023-01-03 14:26:05 -0500

    remove front end workflow and code
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- dashboards-observability/common/constants/custom_panels.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export const CUSTOM_PANELS_API_PREFIX = '/api/observability/operational_panels';
-export const CUSTOM_PANELS_DOCUMENTATION_URL = 'https://opensearch.org/docs/latest/observability-plugin/operational-panels/';
-export const CREATE_PANEL_MESSAGE = 'Enter a name to describe the purpose of this custom panel.';
--- dashboards-observability/public/components/event_analytics/explorer/visualizations/shared_components/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export * from './empty_placeholder';
-export { ToolbarButtonProps, ToolbarButton } from './toolbar_button';
-export { VisCanvassPlaceholder } from './vis_canvass_placeholder'
