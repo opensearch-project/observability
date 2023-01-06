9240a0c64c09a296e140cb398b07f5037dac86f3
Derek Ho <dxho@amazon.com>
2023-01-06 17:28:53 -0500

    remove front end workflow and code (#1362)
    * remove front end workflow and code
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * move folders up one level
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * fix workflow file
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * final workflow fix
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    (cherry picked from commit e7dea87b2008094be7701250547cc02bbb2ca649)
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- dashboards-observability/common/query_manager/ast/expression/index.ts
+++
@@ -1,6 +0,0 @@
-export { AggregateFunction } from './AggregateFunction';
-export { AggregateTerm } from './AggregateTerm';
-export { GroupBy } from './group_by';
-export { Span } from './span';
-export { SpanExpression } from './spanExpression';
-export { Field } from './field';
--- dashboards-observability/public/components/event_analytics/explorer/visualizations/config_panel/index.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export { ConfigPanel } from './config_panel';
