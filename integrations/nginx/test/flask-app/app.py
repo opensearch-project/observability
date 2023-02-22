3ed07177a1dbf84946bd3757283131804ba400be
Simeon Widdis <sawiddis@amazon.com>
2023-02-21 17:25:19 -0800

    Allow live updates to response error rate
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- integrations/nginx/test/flask-app/app.py
+++ integrations/nginx/test/flask-app/app.py
@@ -8,7 +8,7 @@ redis = Redis(host='redis', port=6379)
 
 @app.route("/")
 def hits():
-    if random() < 0.05:
+    if random() < 0.20:
         return "Random error", 500
     h = redis.incr('hits')
     return f"This page has been viewed {h} time(s)"
