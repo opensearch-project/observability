8e5c3e16c43d437982d25c6f240dada6da8007c2
Yaniv Eliash <yaniv@sre.engineer>
2023-03-07 19:59:04 +0200

    * Removed base_integration_path, now new integrations will be created at   the script's path * Changed file and directories path concatanation to os.path.join * Linter kicked in and formatted the file.
    Signed-off-by: Yaniv Eliash <yaniv@sre.engineer>

--- scripts/integrations/main.py
+++ scripts/integrations/main.py
@@ -7,26 +7,26 @@ import shutil
 from rich import print
 
 app = typer.Typer()
-base_integration_path = "../integrations"
 dirs = ["assets", "info", "samples", "schema", "test"]
 
 
 def createIntegrationDirs(directory, force):
     if os.path.isdir(directory):
         if force:
-            force = typer.confirm(f"Are you sure you want to delete {directory}?")
+            force = typer.confirm(
+                f"Are you sure you want to delete {directory}?")
             if force:
                 shutil.rmtree(directory)
                 os.makedirs(directory)
                 for dir in dirs:
-                    os.makedirs(directory + "/" + dir)
+                    os.makedirs(os.path.join(directory, dir))
         else:
             print(f"[red]Directory {directory} already exists[/red]")
             sys.exit(1)
     else:
         os.makedirs(directory)
         for dir in dirs:
-            os.makedirs(directory + "/" + dir)
+            os.makedirs(os.path.join(directory, dir))
 
 
 def populateDefaultVars(var_name):
@@ -52,7 +52,7 @@ def generateJson(directory, integration_name, integration_desc, schema, license)
             "github": "https://github.com/opensearch-project/observability/tree/main/integrarions/"+integration_name
         }}
 
-    with open(directory + '/config.json', 'w', encoding='utf-8') as f:
+    with open(os.path.join(directory, "config.json"), 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
@@ -62,20 +62,26 @@ def error(txt):
 
 
 def main(
-        integration_name: str = typer.Option("", "--name", "-n", help="Your new integration name"),
-        integration_desc: str = typer.Option("", "--description", "-d", help="Your new integration description"),
-        license: str = typer.Option(populateDefaultVars("LICENSE"), "--license", "-l", help="Opensource license (e.g. Apache, MIT)"),
-        data_source: str = typer.Option("", "--data-source", "-s", help="The data source to ingest from, as well as which version(s) to support."),
-        schema: str = typer.Option("", "--schema", "-s", help="Which schema, and at which version, to use. The data will be ingested with this schema."),
-        catalog: str = typer.Option("", "--catalog", "-c", help="The specific catalog to target, such as `observability` or `security`."),
-        force: bool = typer.Option(False, "--force", "-f", help="Force integration directoy removal and recreation")
+        integration_name: str = typer.Option(
+            "", "--name", "-n", help="Your new integration name"),
+        integration_desc: str = typer.Option(
+            "", "--description", "-d", help="Your new integration description"),
+        license: str = typer.Option(populateDefaultVars(
+            "LICENSE"), "--license", "-l", help="Opensource license (e.g. Apache, MIT)"),
+        data_source: str = typer.Option(
+            "", "--data-source", "-s", help="The data source to ingest from, as well as which version(s) to support."),
+        schema: str = typer.Option(
+            "", "--schema", "-s", help="Which schema, and at which version, to use. The data will be ingested with this schema."),
+        catalog: str = typer.Option(
+            "", "--catalog", "-c", help="The specific catalog to target, such as `observability` or `security`."),
+        force: bool = typer.Option(
+            False, "--force", "-f", help="Force integration directoy removal and recreation")
 ):
 
-    directory = integration_name.lower()
-    directory = base_integration_path + "/" + directory.replace("_", "-")
+    directory = integration_name.lower().replace("_", "-")
 
     createIntegrationDirs(directory, force)
-    
+
     generateJson(directory,
                  integration_name,
                  integration_desc,
@@ -84,6 +90,7 @@ def main(
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        typer.run(error("No options been provided\nIf unsure, run again with --help\n"))
+        typer.run(
+            error("No options has been provided\nIf unsure, run again with --help\n"))
     else:
         typer.run(main)
