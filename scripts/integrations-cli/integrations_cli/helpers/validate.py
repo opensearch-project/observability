1a178f3afbfd890b5dab4a40c7acefe174408dc5
Simeon Widdis <sawiddis@amazon.com>
2023-04-13 15:29:07 -0700

    Add work on adding subcomponent validation to integrations
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- scripts/integrations-cli/integrations_cli/helpers/validate.py
+++ scripts/integrations-cli/integrations_cli/helpers/validate.py
@@ -1,5 +1,7 @@
 import jsonschema
 from returns.result import Failure, Result, safe
+from returns.pipeline import is_successful
+import os
 
 from .constants import SCHEMAS
 
@@ -15,9 +17,20 @@ def validate_instance(instance: dict, schema: dict) -> dict:
     return instance
 
 
-def validate_config(config: dict) -> Result[dict, Exception]:
+def validate_config(config: dict, name: str) -> Result[dict, Exception]:
     try:
-        return validate_instance(config, SCHEMAS["integration.schema"])
+        result = validate_instance(config, SCHEMAS["integration.schema"])
+        if not is_successful(result):
+            return result
+        # TODO assumes component is named same thing as mapping
+        # This is not true for e.g. logs
+        # Need to load catalog to see what the expected name is
+        
+        # for component in config["components"]:
+        #     path = os.path.join(f"integrations/{name}/schema", f"{component}.mapping")
+        #     if not os.path.exists(path):
+        #         return Failure(FileNotFoundError(f"Component `{component}` is absent from schema"))
+        return result
     except KeyError as err:
         return Failure(err)
 
