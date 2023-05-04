1dbf8154b13d557261004a0411725a51d1aa677b
Derek Ho <dxho@amazon.com>
2023-05-04 12:38:09 -0400

    push working build
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- src/test/kotlin/org/opensearch/integrations/rest/GetIntegrationIT.kt
+++ src/test/kotlin/org/opensearch/integrations/rest/GetIntegrationIT.kt
@@ -10,22 +10,8 @@ import org.opensearch.integrations.PluginRestTestCase
 import org.opensearch.observability.ObservabilityPlugin.Companion.BASE_INTEGRATIONS_URI
 import org.opensearch.rest.RestRequest
 import org.opensearch.rest.RestStatus
-import java.time.Instant
 
-class GetObjectIT : PluginRestTestCase() {
-    private fun createObject(createRequest: String): String {
-        val createResponse = executeRequest(
-            RestRequest.Method.POST.name,
-            "$BASE_INTEGRATIONS_URI/object",
-            createRequest,
-            RestStatus.OK.status
-        )
-        val id = createResponse.get("objectId").asString
-        Assert.assertNotNull("Id should be generated", id)
-        Thread.sleep(100)
-        return id
-    }
-
+class GetIntegrationIT : PluginRestTestCase() {
 
     fun `test get multiple objects`() {
         val emptyResponse = executeRequest(
@@ -35,6 +21,5 @@ class GetObjectIT : PluginRestTestCase() {
             RestStatus.OK.status
         )
         Assert.assertEquals(0, emptyResponse.get("totalHits").asInt)
-
     }
 }
