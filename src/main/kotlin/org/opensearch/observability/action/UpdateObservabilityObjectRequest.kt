880358de1c7bbba76a678384aa7b02f77db02027
Derek Ho <dxho@amazon.com>
2023-05-18 16:47:17 -0400

    fix ci
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- src/main/kotlin/org/opensearch/observability/action/UpdateObservabilityObjectRequest.kt
+++ src/main/kotlin/org/opensearch/observability/action/UpdateObservabilityObjectRequest.kt
@@ -8,7 +8,7 @@ package org.opensearch.observability.action
 import org.opensearch.action.ActionRequest
 import org.opensearch.action.ActionRequestValidationException
 import org.opensearch.action.ValidateActions
-import org.opensearch.common.Strings
+import org.opensearch.core.common.Strings
 import org.opensearch.common.io.stream.StreamInput
 import org.opensearch.common.io.stream.StreamOutput
 import org.opensearch.common.io.stream.Writeable
