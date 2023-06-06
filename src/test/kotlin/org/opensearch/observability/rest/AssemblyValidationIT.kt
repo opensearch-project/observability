7c5fee31a8a1602d7939d40a20122f86f7eac90d
Simeon Widdis <sawiddis@amazon.com>
2023-06-06 14:35:24 -0700

    Add tests for log dependencies
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- src/test/kotlin/org/opensearch/observability/rest/AssemblyValidationIT.kt
+++ src/test/kotlin/org/opensearch/observability/rest/AssemblyValidationIT.kt
@@ -17,6 +17,30 @@ class AssemblyValidationIT : PluginRestTestCase() {
         private const val LOGS_MAPPING_TEMPLATE_NAME = "ss4o_logs_template"
     }
 
+    fun `test observability communication template was created`() {
+        // verify logs mapping template was created successfully as part of the plugin initialization
+        val response = executeRequest(
+            RestRequest.Method.GET.name,
+            "/_index_template/ss4o_communication_template",
+            "{}",
+            RestStatus.OK.status
+        )
+        Assert.assertNotNull(response.get("index_templates"))
+        Assert.assertNotNull(!response.get("index_templates").asJsonArray.isEmpty)
+    }
+
+    fun `test observability http template was created`() {
+        // verify logs mapping template was created successfully as part of the plugin initialization
+        val response = executeRequest(
+            RestRequest.Method.GET.name,
+            "/_index_template/ss4o_http_template",
+            "{}",
+            RestStatus.OK.status
+        )
+        Assert.assertNotNull(response.get("index_templates"))
+        Assert.assertNotNull(!response.get("index_templates").asJsonArray.isEmpty)
+    }
+
     fun `test observability logs template was created`() {
         // verify logs mapping template was created successfully as part of the plugin initialization
         val response = executeRequest(
