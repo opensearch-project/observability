89938667ea357526e13006500a10d24fd62c5756
Derek Ho <derek01778@gmail.com>
2023-07-13 11:07:49 -0400

    fix 2.9 ci (#1569)
    Signed-off-by: Derek Ho <dxho@amazon.com>
    (cherry picked from commit a28655eb474c202bb82d2ceaaae528729c3fd195)

--- src/main/kotlin/org/opensearch/observability/action/UpdateObservabilityObjectRequest.kt
+++ src/main/kotlin/org/opensearch/observability/action/UpdateObservabilityObjectRequest.kt
@@ -8,13 +8,11 @@ package org.opensearch.observability.action
 import org.opensearch.action.ActionRequest
 import org.opensearch.action.ActionRequestValidationException
 import org.opensearch.action.ValidateActions
-import org.opensearch.common.Strings
 import org.opensearch.common.io.stream.StreamInput
 import org.opensearch.common.io.stream.StreamOutput
 import org.opensearch.common.io.stream.Writeable
 import org.opensearch.common.xcontent.XContentParserUtils
-import org.opensearch.commons.utils.fieldIfNotNull
-import org.opensearch.commons.utils.logger
+import org.opensearch.core.common.Strings
 import org.opensearch.core.xcontent.ToXContent
 import org.opensearch.core.xcontent.ToXContentObject
 import org.opensearch.core.xcontent.XContentBuilder
@@ -24,6 +22,8 @@ import org.opensearch.observability.model.BaseObjectData
 import org.opensearch.observability.model.ObservabilityObjectDataProperties
 import org.opensearch.observability.model.ObservabilityObjectType
 import org.opensearch.observability.model.RestTag.OBJECT_ID_FIELD
+import org.opensearch.observability.util.fieldIfNotNull
+import org.opensearch.observability.util.logger
 import java.io.IOException
 
 /**
