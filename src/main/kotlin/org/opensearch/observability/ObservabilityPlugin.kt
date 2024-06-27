01ba0804afa967e490db5f27fca0141520664757
Craig Perkins <craig5008@gmail.com>
2024-06-27 13:14:22 -0400

    Register system index descriptors through SystemIndexPlugin.getSystemIndexDescriptors (#1837)
    * Register system index descriptors through SystemIndexPlugin.getSystemIndexDescriptors
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Add notebooks index as well
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    * Update baseVersion
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    
    ---------
    
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/main/kotlin/org/opensearch/observability/ObservabilityPlugin.kt
+++ src/main/kotlin/org/opensearch/observability/ObservabilityPlugin.kt
@@ -19,6 +19,7 @@ import org.opensearch.core.common.io.stream.NamedWriteableRegistry
 import org.opensearch.core.xcontent.NamedXContentRegistry
 import org.opensearch.env.Environment
 import org.opensearch.env.NodeEnvironment
+import org.opensearch.indices.SystemIndexDescriptor
 import org.opensearch.observability.action.CreateObservabilityObjectAction
 import org.opensearch.observability.action.DeleteObservabilityObjectAction
 import org.opensearch.observability.action.GetObservabilityObjectAction
@@ -29,6 +30,7 @@ import org.opensearch.observability.resthandler.ObservabilityStatsRestHandler
 import org.opensearch.observability.settings.PluginSettings
 import org.opensearch.plugins.ActionPlugin
 import org.opensearch.plugins.Plugin
+import org.opensearch.plugins.SystemIndexPlugin
 import org.opensearch.repositories.RepositoriesService
 import org.opensearch.rest.RestController
 import org.opensearch.rest.RestHandler
@@ -41,7 +43,7 @@ import java.util.function.Supplier
  * Entry point of the OpenSearch Observability plugin.
  * This class initializes the rest handlers.
  */
-class ObservabilityPlugin : Plugin(), ActionPlugin {
+class ObservabilityPlugin : Plugin(), ActionPlugin, SystemIndexPlugin {
 
     companion object {
         const val PLUGIN_NAME = "opensearch-observability"
@@ -55,6 +57,16 @@ class ObservabilityPlugin : Plugin(), ActionPlugin {
      */
     override fun getSettings(): List<Setting<*>> {
         return PluginSettings.getAllSettings()
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun getSystemIndexDescriptors(settings: Settings): Collection<SystemIndexDescriptor> {
+        return listOf(
+            SystemIndexDescriptor(ObservabilityIndex.INDEX_NAME, "Observability Plugin Configuration index"),
+            SystemIndexDescriptor(ObservabilityIndex.NOTEBOOKS_INDEX_NAME, "Observability Plugin Notebooks index")
+        )
     }
 
     /**
